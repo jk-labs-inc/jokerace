759e47bcca36af317793e7aaaa607d2f8b147fd9
Sean McCaffery <sean.mccaffery9@gmail.com>
2022-02-25 15:55:48 -0500

    add db func exports to hooks index

--- packages/react-app/src/hooks/index.js
+++ packages/react-app/src/hooks/index.js
@@ -1,4 +1,8 @@
 export { default as useDebounce } from "./useDebounce";
 export { default as useLocalStorage } from "./useLocalStorage";
 export { default as useStaticJsonRPC } from "./useStaticJsonRPC";
+export { default as createJoke } from "./createJoke";
+export { default as deleteJoke } from "./deleteJoke";
+export { default as editJoke } from "./editJoke";
+export { default as getAllJokes } from "./getAllJokes";
 export * from "./useContractConfig";
