eb48c4c01ed9d8f97ecbf4d00d9c12ddec3e2fdb
Sean McCaffery <sean.mccaffery9@gmail.com>
2022-02-26 11:49:46 -0500

    add params to db config

--- packages/react-app/src/helpers/db.js
+++ packages/react-app/src/helpers/db.js
@@ -1,6 +1,10 @@
 import faunadb from "faunadb";
 
-const client = new faunadb.Client({ secret: process.env.REACT_APP_FAUNADB_KEY });
+const client = new faunadb.Client({ 
+  secret: process.env.REACT_APP_FAUNADB_KEY,
+  domain: 'db.us.fauna.com',
+  scheme: 'https',
+});
 const q = faunadb.query;
 
 export { client, q };
