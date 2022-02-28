6b2e4319f6823d82c7c197b2eb7d1b7e73795903
Sean McCaffery <sean.mccaffery9@gmail.com>
2022-02-28 01:02:54 -0500

    sort jokes by vote count desc

--- packages/react-app/src/components/RaceItem.jsx
+++ packages/react-app/src/components/RaceItem.jsx
@@ -5,10 +5,20 @@ import { JokeItem } from ".";
 const { Panel } = Collapse;
 
 export default function RaceItem(entry, index) {
+  function voteSortFunc(joke1, joke2) {
+    if (joke1.votes < joke2.votes) {
+      return 1;
+    }
+    if (joke1.votes > joke2.votes) {
+      return -1;
+    }
+    return 0;
+  }
+
   return ( 
     <Panel header={entry.name} key={index}>
       <Collapse>
-        {entry.jokes.map((joke, index) => { return JokeItem(joke, index) } )}
+        {entry.jokes.sort(voteSortFunc).map((joke, index) => { return JokeItem(joke, index) })}
       </Collapse>
     </Panel>
   );
