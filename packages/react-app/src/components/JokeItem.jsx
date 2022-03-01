1ebf4f799923f743383431d25178b59d2f5fdca2
Sean McCaffery <sean.mccaffery9@gmail.com>
2022-02-28 23:37:09 -0500

    clean up, format, and lint

--- packages/react-app/src/components/JokeItem.jsx
+++ packages/react-app/src/components/JokeItem.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Card, Modal, Button, Col, Row } from 'antd';
+import { Card, Modal, Button, Col, Row, Input } from 'antd';
 
 export default function JokeItem({joke}) {
   const [isModalVisible, setIsModalVisible] = useState(false);
@@ -22,15 +22,20 @@ export default function JokeItem({joke}) {
         <Col className="gutter-row" span={12}>
           <h4>{joke.content}</h4>
           <div>Votes: {joke.votes}</div>
+          <div>Author: {joke.author}</div>
         </Col>
         <Col className="gutter-row" span={12}>
           <Button type="primary" onClick={showModal}>
             Vote
           </Button>
           <Modal title="Basic Modal" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
-            <p>Some contents...</p>
-            <p>Some contents...</p>
-            <p>Some contents...</p>
+            {/* TODO: Use this https://ant.design/components/modal/#components-modal-demo-confirm */}
+
+            <p>Your Total Tokens: </p>
+            <p>Remaining Available Votes: </p>
+            <p>Race you are voting in: </p>
+            <p>Joke you are voting for: </p>
+            <Input icon='search' placeholder='Votes to cast for this joke:' />
           </Modal>
         </Col>
       </Row>
--- packages/react-app/src/components/RaceSearch.jsx
+++ packages/react-app/src/components/RaceSearch.jsx
@@ -6,7 +6,7 @@ export default function RaceSearch({races}) {
   const [filteredResults, setFilteredResults] = useState([]);
   const [searchInput, setSearchInput] = useState("");
 
-  function searchItems(searchValue) {
+  function searchRaces(searchValue) {
     setSearchInput(searchValue);
     if (searchInput !== "") {
       const filteredData = races.filter(item => {
@@ -19,12 +19,11 @@ export default function RaceSearch({races}) {
   }
   
   return (
-    <div style={{ border: "1px solid #cccccc", padding: 16, width: 400, margin: "auto", marginTop: 64 }}>
+    <div style={{ border: "1px solid #cccccc", padding: 16, width: 800, margin: "auto", marginTop: 64 }}>
       <Button onClick={() => {window.location.reload();}}>Refresh</Button>
       <Button onClick={() => {window.location.reload();}}>Create Race</Button>
-      <Input icon='search' placeholder='Search...' value={searchInput} onChange={(e) => searchItems(e.target.value)} />
+      <Input icon='search' placeholder='Search races...' value={searchInput} onChange={(e) => searchRaces(e.target.value)} />
       <Card>
-        <h2>Search for your race!</h2>
         {searchInput.length > 1 ? (
           filteredResults.map((entry) => <RaceItem raceData={entry.data} />)
         ) : (
