06eab0a47e85b3719184c52b60a5045116446e16
Sean McCaffery <sean.mccaffery9@gmail.com>
2022-03-01 11:52:25 -0500

    eslint code

--- packages/react-app/src/helpers/db.js
+++ packages/react-app/src/helpers/db.js
@@ -7,37 +7,36 @@ export const client = new faunadb.Client({
 });
 export const q = faunadb.query;
 
-export const getEntireCollectionQuery = (collection) => 
-  client.query(
-    q.Map(
-      q.Paginate(q.Documents(q.Collection(collection))),
-      q.Lambda(x => q.Get(x)),
-    ),
-  )
-  .then(response => {
-    console.log(response)
-    return response.data;
-  })
-  .catch(error => console.log("error", error.message));
+export const getEntireCollectionQuery = collection =>
+  client
+    .query(
+      q.Map(
+        q.Paginate(q.Documents(q.Collection(collection))),
+        q.Lambda(x => q.Get(x)),
+      ),
+    )
+    .then(response => {
+      console.log(response);
+      return response.data;
+    })
+    .catch(error => console.log("error", error.message));
 
-export const createRace = (paramDict) => {
-    const nameParam = paramDict.name;
-    const startTimeParam = paramDict.startTime;
-    const endTimeParam = paramDict.endTime;
+export const createRace = paramDict => {
+  const nameParam = paramDict.name;
+  const startTimeParam = paramDict.startTime;
+  const endTimeParam = paramDict.endTime;
 
-    client.query(
-      q.Create(
-        q.Collection('races'),
-        {
-          data: {
-            name: nameParam,
-            startTime: startTimeParam,
-            endTime: endTimeParam,
-            jokes: []
-          },
+  client
+    .query(
+      q.Create(q.Collection("races"), {
+        data: {
+          name: nameParam,
+          startTime: startTimeParam,
+          endTime: endTimeParam,
+          jokes: [],
         },
-      )
+      }),
     )
     .then(ret => ret)
-    .catch(err => console.warn(err))
-  }
+    .catch(err => console.warn(err));
+};
--- packages/react-app/src/helpers/time.js
+++ packages/react-app/src/helpers/time.js
@@ -1,4 +1,10 @@
 export function convertToUnixTimeStamp(dateObject) {
-  return Date.UTC(dateObject.getUTCFullYear(), dateObject.getUTCMonth(), dateObject.getUTCDate(),
-    dateObject.getUTCHours(), dateObject.getUTCMinutes(), dateObject.getUTCSeconds());
+  return Date.UTC(
+    dateObject.getUTCFullYear(),
+    dateObject.getUTCMonth(),
+    dateObject.getUTCDate(),
+    dateObject.getUTCHours(),
+    dateObject.getUTCMinutes(),
+    dateObject.getUTCSeconds(),
+  );
 }
