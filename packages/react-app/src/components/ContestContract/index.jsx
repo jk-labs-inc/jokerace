45217775b4e7f18f811b70e67d9192caa02c9168
Sean McCaffery <sean.mccaffery9@gmail.com>
2022-03-30 15:27:31 -0400

    update loading screen

--- packages/react-app/src/components/ContestContract/index.jsx
+++ packages/react-app/src/components/ContestContract/index.jsx
@@ -10,34 +10,7 @@ const noContractDisplay = (
   <div>
     Loading...{" "}
     <div style={{ padding: 32 }}>
-      You need to run{" "}
-      <span
-        className="highlight"
-        style={{ marginLeft: 4, /* backgroundColor: "#f1f1f1", */ padding: 4, borderRadius: 4, fontWeight: "bolder" }}
-      >
-        yarn run chain
-      </span>{" "}
-      and{" "}
-      <span
-        className="highlight"
-        style={{ marginLeft: 4, /* backgroundColor: "#f1f1f1", */ padding: 4, borderRadius: 4, fontWeight: "bolder" }}
-      >
-        yarn run deploy
-      </span>{" "}
-      to see your contract here.
-    </div>
-    <div style={{ padding: 32 }}>
-      <span style={{ marginRight: 4 }} role="img" aria-label="warning">
-        ☢️
-      </span>
-      Warning: You might need to run
-      <span
-        className="highlight"
-        style={{ marginLeft: 4, /* backgroundColor: "#f1f1f1", */ padding: 4, borderRadius: 4, fontWeight: "bolder" }}
-      >
-        yarn run deploy
-      </span>{" "}
-      <i>again</i> after the frontend comes up!
+      If this does not populate in a couple of seconds either the contract has not been deployed yet or the address that was input is not correct.
     </div>
   </div>
 );
