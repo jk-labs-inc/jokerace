003fc10590fad620f6e52bea0376cd5fba7e1169
Sean McCaffery <sean.mccaffery9@gmail.com>
2022-05-05 11:34:56 -0400

    clean up

--- packages/react-app/src/components/ContestContract/CsvExportButton.jsx
+++ packages/react-app/src/components/ContestContract/CsvExportButton.jsx
@@ -1,7 +1,6 @@
 import React, { useCallback, useEffect, useState } from "react";
 import { Button } from "antd";
 import { CSVLink, CSVDownload } from "react-csv";
-import { useLookupAddress } from "eth-hooks/dapps/ens";
 
 const CsvExportButton = ({ 
             getAllProposalIdsContractFunction, 
