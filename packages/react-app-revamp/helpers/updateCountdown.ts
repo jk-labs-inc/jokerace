f261b913d71b80df20b43a653201b771709ca0b6
Sean McCaffery <sean.mccaffery9@gmail.com>
2022-07-26 09:29:16 -0400

    ensure start time is never before endtime in updateCountdown

--- packages/react-app-revamp/helpers/updateCountdown.ts
+++ packages/react-app-revamp/helpers/updateCountdown.ts
@@ -1,8 +1,9 @@
 import { differenceInHours, intervalToDuration } from "date-fns";
+import { isBefore } from "date-fns";
 
 export function updateCountdown(countdownEndDatetime: Date) {
   const interval = intervalToDuration({
-    start: new Date(),
+    start: (isBefore(new Date(), countdownEndDatetime) ? new Date() : countdownEndDatetime),
     end: countdownEndDatetime,
   });
 
