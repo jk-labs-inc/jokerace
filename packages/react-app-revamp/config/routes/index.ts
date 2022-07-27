ed9ea87a7af0bfcd2e3d2f78dace455ab783f9db
Naomi Hauret <hauretnaomi@gmail.com>
2022-07-27 13:46:20 +0200

    env: setup new routes for filtering contest (past, ongoing, live)

--- packages/react-app-revamp/config/routes/index.ts
+++ packages/react-app-revamp/config/routes/index.ts
@@ -1,5 +1,8 @@
 export const ROUTE_CREATE_CONTEST = "/contest/new";
 export const ROUTE_VIEW_CONTESTS = "/contests";
+export const ROUTE_VIEW_LIVE_CONTESTS = `${ROUTE_VIEW_CONTESTS}/live`;
+export const ROUTE_VIEW_PAST_CONTESTS = `${ROUTE_VIEW_CONTESTS}/past`;
+export const ROUTE_VIEW_UPCOMING_CONTESTS = `${ROUTE_VIEW_CONTESTS}/upcoming`;
 export const ROUTE_VIEW_CONTEST_BASE_PATH = "/contest/[chain]/[address]";
 export const ROUTE_VIEW_CONTEST = ROUTE_VIEW_CONTEST_BASE_PATH;
 export const ROUTE_VIEW_CONTEST_RULES = `${ROUTE_VIEW_CONTEST}/rules`;
