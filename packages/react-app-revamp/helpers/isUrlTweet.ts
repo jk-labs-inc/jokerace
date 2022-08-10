9de119c6d5b6b48e5e9ce9b2097aa6472f37af51
Sean McCaffery <sean.mccaffery9@gmail.com>
2022-08-10 10:28:41 -0400

    bugfix: only parse as embedded tweet if the proposal is just a link to a tweet alone

--- packages/react-app-revamp/helpers/isUrlTweet.ts
+++ packages/react-app-revamp/helpers/isUrlTweet.ts
@@ -1,6 +1,5 @@
 export function isUrlTweet(str: string) {
   return (
-    str.match(/^https?:\/\/twitter\.com\/(?:#!\/)?(\w+)\/status(es)?\/(\d+)$/) !== null ||
-    (str.includes("twitter.com/") && str.includes("/status/"))
+    str.match(/^https?:\/\/twitter\.com\/(?:#!\/)?(\w+)\/status(es)?\/(\d+)$/) !== null
   );
 }
