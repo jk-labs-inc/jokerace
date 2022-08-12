09b20e8c4764d5a630ce252db3f4b2d844f2c2ba
Sean McCaffery <sean.mccaffery9@gmail.com>
2022-08-12 10:24:42 -0400

    feat: update smart contract abi versioning

--- packages/react-app-revamp/helpers/getContestContractVersion.ts
+++ packages/react-app-revamp/helpers/getContestContractVersion.ts
@@ -1,5 +1,7 @@
 import DeployedContestContract from "@contracts/bytecodeAndAbi/Contest.sol/Contest.json";
-import LegacyDeployedContestContract from "@contracts/bytecodeAndAbi/Contest.legacy.sol/Contest.json";
+import LegacyDeployedContestContract from "@contracts/bytecodeAndAbi/Contest.legacy.1.pre-prompt.sol/Contest.json";
+import PromptDeployedContestContract from "@contracts/bytecodeAndAbi/Contest.legacy.2.prompt.sol/Contest.json";
+import AllProposalTotalVotesDeployedContestContract from "@contracts/bytecodeAndAbi/Contest.legacy.3.allProposalTotalVotes.sol/Contest.json";
 import { getProvider } from "@wagmi/core";
 import { utils } from "ethers";
 
@@ -8,11 +10,15 @@ export async function getContestContractVersion(address: string) {
   const bytecode = await provider.getCode(address);
 
   if (bytecode.length <= 2) return null;
-  if (bytecode.includes(utils.id("prompt()").slice(2, 10))) {
-    return DeployedContestContract.abi;
+  if (!bytecode.includes(utils.id("prompt()").slice(2, 10))) {
+    return LegacyDeployedContestContract.abi;
+  } else if (!bytecode.includes(utils.id("allProposalTotalVotes()").slice(2, 10))) {
+    return PromptDeployedContestContract.abi;
+  } else if (!bytecode.includes(utils.id("downvotingAllowed()").slice(2, 10))) {
+    return AllProposalTotalVotesDeployedContestContract.abi;
   }
-
-  return LegacyDeployedContestContract.abi;
+  
+  return DeployedContestContract.abi;
 }
 
 export default getContestContractVersion;
