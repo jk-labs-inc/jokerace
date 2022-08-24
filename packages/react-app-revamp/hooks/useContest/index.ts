2a60965064398700993b24f311199d82141cdc0c
naomiHauret <hauretnaomi@gmail.com>
2022-08-24 08:42:19 +0200

    typo: fix typo

--- packages/react-app-revamp/hooks/useContest/index.ts
+++ packages/react-app-revamp/hooks/useContest/index.ts
@@ -270,7 +270,7 @@ export function useContest() {
       if (
         process.env.NEXT_PUBLIC_SUPABASE_URL !== "" &&
         process.env.NEXT_PUBLIC_SUPABASE_URL &&
-        process.env.NEXT_getCurrentSubmissionTokenVotesPUBLIC_SUPABASE_ANON_KEY !== "" &&
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY !== "" &&
         process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
       ) {
         const config = await import("@config/supabase");
