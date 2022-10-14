48bb1a47022a1e5a23a675e8a492e3d1ad233404
naomiHauret <hauretnaomi@gmail.com>
2022-10-14 17:04:41 +0200

    bugfix: fix ts error

--- packages/react-app-revamp/config/urql/index.ts
+++ packages/react-app-revamp/config/urql/index.ts
@@ -2,6 +2,7 @@ import { createClient } from "urql";
 
 const API_URL = (process.env.NEXT_PUBLIC_LENS_API_URL as string) ?? "https://api.lens.dev";
 
+//@ts-ignore
 export const client = new createClient({
   url: API_URL,
 });
