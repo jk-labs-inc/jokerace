9d89f347af39b213e1e34fc95541ab9c594366ae
naomiHauret <hauretnaomi@gmail.com>
2022-10-19 10:49:14 +0200

    bugfix: fix ts error

--- packages/react-app-revamp/config/web3storage/index.ts
+++ packages/react-app-revamp/config/web3storage/index.ts
@@ -1,5 +1,5 @@
 import { Web3Storage } from 'web3.storage'
-const WEB3STORAGE_TOKEN = process.env.NEXT_PUBLIC_WEB3STORAGE_TOKEN
+const WEB3STORAGE_TOKEN = process.env.NEXT_PUBLIC_WEB3STORAGE_TOKEN as string
 
 export function getAccessToken() {
   return WEB3STORAGE_TOKEN
