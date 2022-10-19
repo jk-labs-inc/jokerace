7e1c3e45e9c44688d355798334805e1669e724cf
naomiHauret <hauretnaomi@gmail.com>
2022-10-19 10:46:29 +0200

    wip: implement rewards

--- packages/react-app-revamp/helpers/getContestContractVersion.ts
+++ packages/react-app-revamp/helpers/getContestContractVersion.ts
@@ -2,7 +2,9 @@ import DeployedContestContract from "@contracts/bytecodeAndAbi/Contest.sol/Conte
 import LegacyDeployedContestContract from "@contracts/bytecodeAndAbi/Contest.legacy.1.pre-prompt.sol/Contest.json";
 import PromptDeployedContestContract from "@contracts/bytecodeAndAbi/Contest.legacy.2.prompt.sol/Contest.json";
 import AllProposalTotalVotesDeployedContestContract from "@contracts/bytecodeAndAbi/Contest.legacy.3.allProposalTotalVotes.sol/Contest.json";
-import ProposalVotesDownvotes from "@contracts/bytecodeAndAbi/Contest.legacy.4.proposalVotesDownvotes.sol/Contest.json";
+import ProposalVotesDownvotesContract from "@contracts/bytecodeAndAbi/Contest.legacy.4.proposalVotesDownvotes.sol/Contest.json";
+import SubmissionTokenGatingContract from "@contracts/bytecodeAndAbi/Contest.legacy.5.submissionTokenGating.sol/Contest.json"
+
 import { getProvider } from "@wagmi/core";
 import { utils } from "ethers";
 import { chains } from "@config/wagmi";
@@ -21,9 +23,11 @@ export async function getContestContractVersion(address: string, chainName: stri
   } else if (!bytecode.includes(utils.id("downvotingAllowed()").slice(2, 10))) {
     return AllProposalTotalVotesDeployedContestContract.abi;
   } else if (!bytecode.includes(utils.id("submissionGatingByVotingToken()").slice(2, 10))) {
-    return ProposalVotesDownvotes.abi;
+    return ProposalVotesDownvotesContract.abi;
   }
-  
+  else if (!bytecode.includes(utils.id("officialRewardsModule()").slice(2, 10))) {
+    return SubmissionTokenGatingContract.abi;
+  }
   return DeployedContestContract.abi;
 }
 
