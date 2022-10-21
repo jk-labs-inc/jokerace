8e2e45a0fb282f3c3ed0a0fd93d446a4e40db0cf
Sean McCaffery <sean.mccaffery9@gmail.com>
2022-10-21 10:48:36 -0400

    feat: add RewardsModuleContractVersion versioning helper

---
+++ packages/react-app-revamp/helpers/getRewardsModuleContractVersion.ts
@@ -0,0 +1,17 @@
+import DeployedRewardsModuleContract from "@contracts/bytecodeAndAbi/modules/RewardsModule.sol/RewardsModule.json";
+import { getProvider } from "@wagmi/core";
+import { utils } from "ethers";
+import { chains } from "@config/wagmi";
+
+export async function getRewardsModuleContractVersion(address: string, chainName: string) {
+  const chainId = chains
+      .filter(chain => chain.name.toLowerCase().replace(" ", "") === chainName)?.[0]?.id;;
+  const provider = await getProvider({chainId: chainId});
+  const bytecode = await provider.getCode(address);
+
+  if (bytecode.length <= 2) return null;
+  
+  return DeployedRewardsModuleContract.abi;
+}
+
+export default getRewardsModuleContractVersion;
