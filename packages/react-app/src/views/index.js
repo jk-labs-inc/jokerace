1f23290a481d81df2082da7fa9cd1259a2b232e9
Sean McCaffery <sean.mccaffery9@gmail.com>
2023-02-17 19:35:00 -0500

    chore: remove v1 package (still available on v1 branch

--- packages/react-app/src/views/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Home } from "./Home";
-export { default as ExampleUI } from "./ExampleUI";
-export { default as ContestsPage } from "./ContestsPage";
-export { default as Hints } from "./Hints";
-export { default as Subgraph } from "./Subgraph";
