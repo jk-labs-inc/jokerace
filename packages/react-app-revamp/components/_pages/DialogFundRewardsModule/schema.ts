d5cb552107fe4081947a6bf5a69974b963037615
Blazo <medenicablazo@gmail.com>
2023-03-22 18:05:22 +0100

    bug: fund rewards with matic or other native token (#197)
    * bug: create separate component for sending funds to pool and fix bug
    
    * bug: move back button to form component

--- packages/react-app-revamp/components/_pages/DialogFundRewardsModule/schema.ts
+++ packages/react-app-revamp/components/_pages/DialogFundRewardsModule/schema.ts
@@ -3,7 +3,7 @@ import { object, string, number, boolean } from "zod";
 export const schema = object({
   tokenRewardsAddress: string()
     .regex(/^0x[a-fA-F0-9]{40}$/)
-    .or(string().max(0)),
+    .or(string().nullish()),
   isErc20: boolean(),
   amount: number().positive(),
 });
