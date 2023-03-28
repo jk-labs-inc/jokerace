cc143abc00367e5c2badc81acde1547c6decdaff
Tim Black <timothy.william.black@gmail.com>
2023-03-28 09:29:09 -0700

    adding AVAX for flipside reasons

---
+++ packages/react-app-revamp/config/wagmi/custom-chains/avax.ts
@@ -0,0 +1,19 @@
+export const avaxMainnet = {
+	id: 43114,
+	name: 'Avalanche',
+	network: 'AVAX C-Chain',
+	nativeCurrency: {
+	  decimals: 18,
+	  name: 'AVAX',
+	  symbol: 'AVAX',
+	},
+	rpcUrls: {
+	  public: 'https://api.avax.network/ext/bc/C/rpc',
+	  default: 'https://api.avax.network/ext/bc/C/rpc',
+	},
+	blockExplorers: {
+	  escan: { name: 'Snowtrace', url: 'https://snowtrace.io/' },
+	  default: { name: 'Snowtracee', url: 'https://snowtrace.io/' },
+	},
+  }
+  
