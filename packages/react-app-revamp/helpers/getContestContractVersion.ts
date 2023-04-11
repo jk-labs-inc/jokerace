b7f088d885b6605045591dc617ab37de1f124fd5
Sean McCaffery <sean.mccaffery9@gmail.com>
2023-04-11 12:28:31 -0400

    feat: add numbered versioning pt 1

--- packages/react-app-revamp/helpers/getContestContractVersion.ts
+++ packages/react-app-revamp/helpers/getContestContractVersion.ts
@@ -1,9 +1,10 @@
-import LegacyDeployedContestContract from "@contracts/bytecodeAndAbi/Contest.legacy.1.pre-prompt.sol/Contest.json";
-import PromptDeployedContestContract from "@contracts/bytecodeAndAbi/Contest.legacy.2.prompt.sol/Contest.json";
-import AllProposalTotalVotesDeployedContestContract from "@contracts/bytecodeAndAbi/Contest.legacy.3.allProposalTotalVotes.sol/Contest.json";
-import ProposalVotesDownvotesContract from "@contracts/bytecodeAndAbi/Contest.legacy.4.proposalVotesDownvotes.sol/Contest.json";
-import SubmissionTokenGatingContract from "@contracts/bytecodeAndAbi/Contest.legacy.5.submissionTokenGating.sol/Contest.json";
-import RewardsContract from "@contracts/bytecodeAndAbi/Contest.legacy.6.rewards.sol/Contest.json";
+import LegacyDeployedContestContract from "@contracts/bytecodeAndAbi/Contest.2.1.pre-prompt.sol/Contest.json";
+import PromptDeployedContestContract from "@contracts/bytecodeAndAbi/Contest.2.2.prompt.sol/Contest.json";
+import AllProposalTotalVotesDeployedContestContract from "@contracts/bytecodeAndAbi/Contest.2.3.allProposalTotalVotes.sol/Contest.json";
+import ProposalVotesDownvotesContract from "@contracts/bytecodeAndAbi/Contest.2.4.proposalVotesDownvotes.sol/Contest.json";
+import SubmissionTokenGatingContract from "@contracts/bytecodeAndAbi/Contest.2.5.submissionTokenGating.sol/Contest.json";
+import RewardsContract from "@contracts/bytecodeAndAbi/Contest.2.6.rewards.sol/Contest.json";
+import NoTiesFoundContract from "@contracts/bytecodeAndAbi/Contest.2.7.noTiesFound.sol/Contest.json";
 import DeployedContestContract from "@contracts/bytecodeAndAbi/Contest.sol/Contest.json";
 
 import { chains } from "@config/wagmi";
@@ -15,6 +16,9 @@ export async function getContestContractVersion(address: string, chainName: stri
   const provider = getProvider({ chainId: chainId });
   const bytecode = await provider.getCode(address);
 
+  // TODO: Add logic "check version + if 2.7, use NoTiesFound"
+
+  // Here to catch contract versions that aren't versioned correctly with numbers
   if (bytecode.length <= 2) return null;
   if (!bytecode.includes(utils.id("prompt()").slice(2, 10))) {
     return LegacyDeployedContestContract.abi;
@@ -27,6 +31,9 @@ export async function getContestContractVersion(address: string, chainName: stri
   } else if (!bytecode.includes(utils.id("officialRewardsModule()").slice(2, 10))) {
     return SubmissionTokenGatingContract.abi;
   }
+
+  // If none of the cases are found (this shouldn't happen if we have been correctly versioning when we ship smart cntract code),
+  // then just use the latest bytecode and ABI.
   return DeployedContestContract.abi;
 }
 
--- packages/react-app-revamp/helpers/getRewardsModuleContractVersion.ts
+++ packages/react-app-revamp/helpers/getRewardsModuleContractVersion.ts
@@ -1,4 +1,5 @@
-import LegacyDeployedRewardsModuleContract from "@contracts/bytecodeAndAbi/modules/RewardsModule.legacy.1.first.sol/RewardsModule.json";
+import LegacyDeployedRewardsModuleContract from "@contracts/bytecodeAndAbi/modules/RewardsModule.2.1.first.sol/RewardsModule.json";
+import NoTiesFoundContract from "@contracts/bytecodeAndAbi/modules/RewardsModule.2.2.noTiesFound.sol/RewardsModule.json";
 import DeployedRewardsModuleContract from "@contracts/bytecodeAndAbi/modules/RewardsModule.sol/RewardsModule.json";
 
 import { chains } from "@config/wagmi";
@@ -8,10 +9,11 @@ import { utils } from "ethers";
 export async function getRewardsModuleContractVersion(address: string, chainName: string) {
   const chainId = chains.filter(chain => chain.name.toLowerCase().replace(" ", "") === chainName)?.[0]?.id;
   const provider = await getProvider({ chainId: chainId });
-  const bytecode = await provider.getCode(address);
 
-  if (bytecode.length <= 2) return null;
+  // TODO: Add logic "if version call comes back with 1 or the method doesn't exist, then use 2.1, else, use the relevant version"
 
+  // If none of the cases are found (this shouldn't happen if we have been correctly versioning when we ship smart cntract code),
+  // then just use the latest bytecode and ABI.
   return DeployedRewardsModuleContract.abi;
 }
 
