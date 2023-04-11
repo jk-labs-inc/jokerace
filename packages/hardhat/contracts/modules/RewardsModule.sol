76743eef54c6b325350847b5d83c547cfc3ce99c
Sean McCaffery <sean.mccaffery9@gmail.com>
2023-04-11 13:18:40 -0400

    chore: correctly version current version, 2.8

--- packages/hardhat/contracts/modules/RewardsModule.sol
+++ packages/hardhat/contracts/modules/RewardsModule.sol
@@ -91,7 +91,7 @@ contract RewardsModule is Context {
      * @dev Version of the rewards module. Default: "1"
      */
     function version() public view virtual returns (string memory) {
-        return "2.2";
+        return "2.3";
     }
 
     /**
