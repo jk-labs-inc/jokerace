c9fd4c2a42d3a62bf6574700f9cdc81f64397cdd
Sean McCaffery <sean.mccaffery9@gmail.com>
2023-04-12 09:00:31 -0400

    chore: version unversioned commit (#241)
    * chore: version unversioned commit
    
    * chore: clean up getVersion hooks

--- packages/react-app-revamp/helpers/getRewardsModuleContractVersion.ts
+++ packages/react-app-revamp/helpers/getRewardsModuleContractVersion.ts
@@ -1,6 +1,9 @@
+import LegacyDeployedRewardsModuleContract from "@contracts/bytecodeAndAbi/modules/RewardsModule.legacy.1.first.sol/RewardsModule.json";
 import DeployedRewardsModuleContract from "@contracts/bytecodeAndAbi/modules/RewardsModule.sol/RewardsModule.json";
-import { getProvider } from "@wagmi/core";
+
 import { chains } from "@config/wagmi";
+import { getProvider } from "@wagmi/core";
+import { utils } from "ethers";
 
 export async function getRewardsModuleContractVersion(address: string, chainName: string) {
   const chainId = chains.filter(chain => chain.name.toLowerCase().replace(" ", "") === chainName)?.[0]?.id;
