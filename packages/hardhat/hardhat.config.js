d469d6fc4f554e117ebd7542a2cf3705f4267b74
Sean McCaffery <sean.mccaffery9@gmail.com>
2023-04-14 17:45:33 -0400

    enhancement: add hardhat-contract-sizer

--- packages/hardhat/hardhat.config.js
+++ packages/hardhat/hardhat.config.js
@@ -12,6 +12,8 @@ require("hardhat-gas-reporter");
 require("@nomiclabs/hardhat-ethers");
 require("@nomiclabs/hardhat-etherscan");
 
+require("hardhat-contract-sizer");
+
 const { isAddress, getAddress, formatUnits, parseUnits } = utils;
 
 /*
