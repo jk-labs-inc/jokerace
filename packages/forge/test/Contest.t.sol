11c1f4f0d5c88fb89a6445c83d84c0cf10b16024
Sean McCaffery <sean.mccaffery9@gmail.com>
2023-04-16 09:39:03 -0400

    chore: fix tests and add testVote

--- packages/forge/test/Contest.t.sol
+++ packages/forge/test/Contest.t.sol
@@ -10,12 +10,13 @@ contract ContestTest is Test {
     bytes32[] proof;
 
     function setUp() public {
-        numParams.push(0); // _initialDownvotingAllowed
-        numParams.push(100); // _initialMaxProposalCount
+        numParams.push(1681650000); // _initialContestStart
+        numParams.push(10000); // _initialVotingDelay
+        numParams.push(10000); // _initialVotingPeriod,
         numParams.push(2); // _initialNumAllowedProposalSubmissions,
-        numParams.push(1696614513); // _initialVotingPeriod,
-        numParams.push(1691614513); // _initialVotingDelay
-        numParams.push(1581456009); // _initialContestStart
+        numParams.push(100); // _initialMaxProposalCount
+        numParams.push(0); // _initialDownvotingAllowed
+        emit log_array(numParams);
         contest = new Contest("test",
                               "hello world",
                               bytes32(0xd0aa6a4e5b4e13462921d7518eebdb7b297a7877d6cfe078b0c318827392fb55),
@@ -24,7 +25,7 @@ contract ContestTest is Test {
     }
 
     /*
-        For the below merkle tree:
+        For this merkle tree:
         {
             "decimals": 18,
             "airdrop": {
@@ -35,9 +36,29 @@ contract ContestTest is Test {
     */
     function testPropose() public {
         proof = [bytes32(0x005a0033b5a1ac5c2872d7689e0f064ad6d2287ab98439e44c822e1c46530033)];
+
         vm.startPrank(address(0x016C8780e5ccB32E5CAA342a926794cE64d9C364));
+
+        vm.warp(1681650001);
         uint256 proposalId = contest.propose("proposalDescription", proof);
+
         vm.stopPrank();
+
         assertEq(proposalId, 97841183246694117049131835047498378131093565157767228515338175708860829108582);
+    }
+
+    function testVote() public {
+        proof = [bytes32(0x005a0033b5a1ac5c2872d7689e0f064ad6d2287ab98439e44c822e1c46530033)];
+
+        vm.startPrank(address(0x016C8780e5ccB32E5CAA342a926794cE64d9C364));
+
+        vm.warp(1681650001);
+        uint256 proposalId = contest.propose("proposalDescription", proof);
+        vm.warp(1681660001);
+        uint256 totalVotes = contest.castVote(proposalId, 0, 10000000000000000000, 1000000000000000000, proof);
+
+        vm.stopPrank();
+
+        assertEq(totalVotes, 10000000000000000000);
     }
 }
