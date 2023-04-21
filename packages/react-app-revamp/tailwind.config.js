36132b06f0d2c94865af274bd046d4b6ce0bafde
blazh <medenicablazo@gmail.com>
2023-04-21 14:19:09 +0200

    refactor: update old styles

--- packages/react-app-revamp/tailwind.config.js
+++ packages/react-app-revamp/tailwind.config.js
@@ -242,11 +242,11 @@ module.exports = {
         },
         appear: {
           from: {
-            opacity: 0,
+            opacity: "0",
             transform: "translateY(10px)",
           },
           to: {
-            opacity: 1,
+            opacity: "1",
             transform: "translateY(0)",
           },
         },
