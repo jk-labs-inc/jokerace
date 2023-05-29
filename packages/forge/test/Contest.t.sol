b956edb25734f0b30e94c8e763b5a0d756976d7b
Sean McCaffery <sean.mccaffery9@gmail.com>
2023-05-29 16:29:27 -0400

    refactor: use ether solidity keyword

--- packages/forge/test/Contest.t.sol
+++ packages/forge/test/Contest.t.sol
@@ -12,7 +12,6 @@ contract ContestTest is Test {
     uint64 public constant NUM_ALLOWED_PROPOSAL_SUBMISSIONS = 2;
     uint64 public constant MAX_PROPOSAL_COUNT = 100;
     uint64 public constant DOWNVOTING_ALLOWED = 0;
-    uint256 public constant ONE_ETH_IN_WEI = 1000000000000000000;
     uint256[] public numParams = [
         CONTEST_START,
         VOTING_DELAY,
@@ -162,11 +161,11 @@ contract ContestTest is Test {
         vm.warp(1681650001);
         uint256 proposalId = contest.propose(firstProposalPA1, proof1);
         vm.warp(1681660001);
-        uint256 totalVotes = contest.castVote(proposalId, 0, 10 * ONE_ETH_IN_WEI, 1 * ONE_ETH_IN_WEI, proof1);
+        uint256 totalVotes = contest.castVote(proposalId, 0, 10 ether, 1 ether, proof1);
 
         vm.stopPrank();
 
-        assertEq(totalVotes, 10 * ONE_ETH_IN_WEI);
+        assertEq(totalVotes, 10 ether);
     }
 
     function testVoteWithoutProof() public {
@@ -175,11 +174,11 @@ contract ContestTest is Test {
         vm.warp(1681650001);
         uint256 proposalId = contest.propose(firstProposalPA1, proof1);
         vm.warp(1681660001);
-        contest.castVote(proposalId, 0, 10 * ONE_ETH_IN_WEI, 1 * ONE_ETH_IN_WEI, proof1);
-        uint256 totalVotesWithoutProof = contest.castVoteWithoutProof(proposalId, 0, 1 * ONE_ETH_IN_WEI);
+        contest.castVote(proposalId, 0, 10 ether, 1 ether, proof1);
+        uint256 totalVotesWithoutProof = contest.castVoteWithoutProof(proposalId, 0, 1 ether);
 
         vm.stopPrank();
 
-        assertEq(totalVotesWithoutProof, 10 * ONE_ETH_IN_WEI);
+        assertEq(totalVotesWithoutProof, 10 ether);
     }
 }
