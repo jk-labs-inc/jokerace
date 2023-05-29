110b7de7c28197e7b743c01e62c56c4be4ffbbdf
blazh <medenicablazo@gmail.com>
2023-05-29 21:18:21 +0200

    refactor: update back icon on submit as well

--- packages/react-app-revamp/components/_pages/Create/components/Buttons/Submit/index.tsx
+++ packages/react-app-revamp/components/_pages/Create/components/Buttons/Submit/index.tsx
@@ -54,7 +54,7 @@ const CreateContestButton: FC<CreateContestButtonProps> = ({ step, onClick }) =>
         {step > 1 && (
           <div className="flex items-center gap-[2px] md:-ml-[15px] cursor-pointer group" onClick={onPreviousStep}>
             <div className="transition-transform duration-200 group-hover:-translate-x-1">
-              <Image src="/create-flow/back.png" alt="back" width={20} height={18} className="mt-[1px]" />
+              <Image src="/create-flow/back.svg" alt="back" width={15} height={15} className="mt-[1px]" />
             </div>
             <p className="text-[16px]">back</p>
           </div>
