14ddb2541b714014bb8298f4e53153063157d284
blazh <medenicablazo@gmail.com>
2023-05-30 13:01:10 +0200

    feat: add different placeholder for mobile/destkop

--- packages/react-app-revamp/components/_pages/Create/pages/ContestSummary/index.tsx
+++ packages/react-app-revamp/components/_pages/Create/pages/ContestSummary/index.tsx
@@ -1,4 +1,5 @@
 import { useDeployContestStore } from "@hooks/useDeployContest/store";
+import { useMedia } from "react-use";
 import CreateNextButton from "../../components/Buttons/Next";
 import ErrorMessage from "../../components/Error";
 import StepCircle from "../../components/StepCircle";
@@ -8,11 +9,18 @@ import { CONTEST_TITLE_MAX_LENGTH, CONTEST_TITLE_MIN_LENGTH } from "../../consta
 import { useNextStep } from "../../hooks/useNextStep";
 import { validationFunctions } from "../../utils/validation";
 
+// Define your mobile and desktop placeholders.
+const MOBILE_PLACEHOLDER = "eg. “submit a project” “propose a delegate”";
+const DESKTOP_PLACEHOLDER = "eg. “submit a project” “propose a delegate” “predict the market”";
+
 const CreateContestSummary = () => {
   const { summary, setSummary, step, errors } = useDeployContestStore(state => state);
   const currentStepError = errors.find(error => error.step === step);
   const summaryValidation = validationFunctions.get(step);
   const onNextStep = useNextStep([() => summaryValidation?.[0].validation(summary)]);
+  const isMobile = useMedia("(max-width: 768px)");
+
+  const placeholderText = isMobile ? MOBILE_PLACEHOLDER : DESKTOP_PLACEHOLDER;
 
   const handleSummaryChange = (value: string) => {
     setSummary(value);
@@ -28,7 +36,7 @@ const CreateContestSummary = () => {
         <CreateTextInput
           className="w-full md:w-[600px]"
           value={summary}
-          placeholder="eg. “submit a project” “propose a delegate” “predict the market”"
+          placeholder={placeholderText}
           minLength={CONTEST_TITLE_MIN_LENGTH}
           maxLength={CONTEST_TITLE_MAX_LENGTH}
           onChange={value => handleSummaryChange(value)}
