dec7e2341f49b633ef1aead1143744bc1b9a3672
Sean McCaffery <sean.mccaffery9@gmail.com>
2023-06-26 09:14:17 -0400

    chore: add check to proposeWithoutProof

--- packages/forge/src/governance/Governor.sol
+++ packages/forge/src/governance/Governor.sol
@@ -265,7 +265,10 @@ abstract contract Governor is Context, ERC165, EIP712, GovernorMerkleVotes, IGov
      * @dev See {IGovernor-proposeWithoutProof}.
      */
     function proposeWithoutProof(ProposalCore memory proposal) public virtual override returns (uint256) {
-        require(addressSubmitterVerified[msg.sender], "Governor: address is not permissioned to submit");
+        if (submissionMerkleRoot != 0) {
+            // if the submission root is 0, then anyone can submit; otherwise, this address needs to have been verified
+            require(addressSubmitterVerified[msg.sender], "Governor: address is not permissioned to submit");
+        }
         validateProposalData(proposal);
         return _castProposal(proposal);
     }
