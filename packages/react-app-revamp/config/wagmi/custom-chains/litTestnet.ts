a13eaed1a1f22e15f2ada476a16b624152b4a38a
Sean McCaffery <sean.mccaffery9@gmail.com>
2023-06-30 10:57:20 -0400

    chain: add lit testnet (#297)
    * chain: add lit testnet
    
    * chain: add lit testnet pt 2

---
+++ packages/react-app-revamp/config/wagmi/custom-chains/litTestnet.ts
@@ -0,0 +1,18 @@
+export const litTestnet = {
+  id: 175177,
+  name: 'chroniclelittestnet',
+  network: 'chroniclelittestnet',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'Lit',
+    symbol: 'LIT',
+  },
+  rpcUrls: {
+    public: 'https://chain-rpc.litprotocol.com/http',
+    default: 'https://chain-rpc.litprotocol.com/http',
+  },
+  blockExplorers: {
+    etherscan: { name: 'Chronicle Block Explorer', url: 'https://chain.litprotocol.com/' },
+    default: { name: 'Chronicle Block Explorer', url: 'https://chain.litprotocol.com/' },
+  },
+}
