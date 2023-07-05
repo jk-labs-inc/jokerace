d515fc3abfaa41de7c2503a93d84d85c00310310
Sean McCaffery <sean.mccaffery9@gmail.com>
2023-07-04 22:04:48 -0400

    docs: clarify dupe note docs

--- packages/react-app-revamp/components/_pages/Create/pages/ContestSubmission/components/SubmissionAllowlist/index.tsx
+++ packages/react-app-revamp/components/_pages/Create/pages/ContestSubmission/components/SubmissionAllowlist/index.tsx
@@ -74,7 +74,7 @@ const CreateSubmissionAllowlist = () => {
           copy-paste allowlist into preview box (up to 100 line items) or upload a csv below <br />
         </p>
         <p className="text-[16px] text-neutral-11">
-          Please note: there must be no duplicates in the allowlist <br />
+          Please note: there must be no duplicate addresses in the allowlist <br />
         </p>
       </div>
       <CSVEditorSubmission onChange={onAllowListChange} />
