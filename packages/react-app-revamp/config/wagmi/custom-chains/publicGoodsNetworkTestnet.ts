834215f1d4cab5243a44804ef0fda8061442324e
Sean McCaffery <sean.mccaffery9@gmail.com>
2023-07-06 12:51:36 -0400

    chain: add public goods network testnet (#346)

---
+++ packages/react-app-revamp/config/wagmi/custom-chains/publicGoodsNetworkTestnet.ts
@@ -0,0 +1,18 @@
+export const publicGoodsNetworkTestnet = {
+  id: 58008,
+  name: 'publicGoodsNetworkTestnet',
+  network: 'publicGoodsNetworkTestnet',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'Ether',
+    symbol: 'ETH',
+  },
+  rpcUrls: {
+    public: 'https://sepolia.publicgoods.network',
+    default: 'https://sepolia.publicgoods.network',
+  },
+  blockExplorers: {
+    etherscan: { name: 'Public Goods Network Testnet Block Explorer', url: 'https://explorer.sepolia.publicgoods.network/' },
+    default: { name: 'Public Goods Network Testnet Block Explorer', url: 'https://explorer.sepolia.publicgoods.network/' },
+  },
+}
