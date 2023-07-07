2f2f57b30bc5feb19eeb36bfe5261037d9eb95c9
Sean McCaffery <sean.mccaffery9@gmail.com>
2023-07-07 08:17:28 -0400

    chain: add near aurora

---
+++ packages/react-app-revamp/config/wagmi/custom-chains/nearAuroraTestnet.ts
@@ -0,0 +1,18 @@
+export const nearAuroraTestnet = {
+  id: 1313161555,
+  name: 'nearAuroraTestnet',
+  network: 'nearAuroraTestnet',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'Ether',
+    symbol: 'ETH',
+  },
+  rpcUrls: {
+    public: 'https://testnet.aurora.dev',
+    default: 'https://testnet.aurora.dev',
+  },
+  blockExplorers: {
+    etherscan: { name: 'Near Aurora Testnet Block Explorer', url: 'https://testnet.aurorascan.dev/' },
+    default: { name: 'Near Aurora Testnet Block Explorer', url: 'https://testnet.aurorascan.dev/' },
+  },
+}
