01eecd9c1f722ff6399101655a62e72e3a633c93
Sean McCaffery <sean.mccaffery9@gmail.com>
2023-07-24 17:07:00 -0400

    bug: remove alchemy rpc hook, just use custom configs

--- packages/react-app-revamp/config/wagmi/custom-chains/polygonMumbai.ts
+++ packages/react-app-revamp/config/wagmi/custom-chains/polygonMumbai.ts
@@ -9,7 +9,7 @@ export const polygonMumbai = {
   },
   rpcUrls: {
     public: 'https://rpc.ankr.com/polygon_mumbai',
-    default: 'https://rpc.ankr.com/polygon_mumbai',
+    default: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_KEY}`,
   },
   blockExplorers: {
     etherscan: { name: 'Polygon Mumbai Etherscan', url: 'https://mumbai.polygonscan.com/' },
--- packages/react-app-revamp/config/wagmi/custom-chains/polygonZkMainnet.ts
+++ packages/react-app-revamp/config/wagmi/custom-chains/polygonZkMainnet.ts
@@ -9,7 +9,7 @@ export const polygonZkMainnet = {
   },
   rpcUrls: {
     public: 'https://zkevm-rpc.com',
-    default: 'https://zkevm-rpc.com',
+    default: `https://polygonzkevm-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_KEY}`,
   },
   blockExplorers: {
     etherscan: { name: 'Polygon zkEvm Mainnet Scan', url: 'https://zkevm.polygonscan.com/' },
