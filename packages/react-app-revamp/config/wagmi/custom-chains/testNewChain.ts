33bd33a2682911b1a5a43e41a2f118f4589302de
Sean McCaffery <sean.mccaffery9@gmail.com>
2023-08-03 13:58:27 -0400

    test new chain add

---
+++ packages/react-app-revamp/config/wagmi/custom-chains/testNewChain.ts
@@ -0,0 +1,18 @@
+export const test = {
+  id: 11155111,
+  name: 'test',
+  network: 'test',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'Ether',
+    symbol: 'ETH',
+  },
+  rpcUrls: {
+    public: 'https://rpc.sepolia.org/',
+    default: 'https://rpc.sepolia.org/',
+  },
+  blockExplorers: {
+    etherscan: { name: 'Sepolia Etherscan', url: 'https://sepolia.etherscan.io' },
+    default: { name: 'Sepolia Otterscan', url: 'https://sepolia.otterscan.io' },
+  },
+}
