450a0368a9021cda5715de4eaad5bab6262846e1
zho <medenicablazo@gmail.com>
2023-08-11 14:23:55 +0200

    chore: refactor and organize markdown code, prevent more than 3 list items in a preview box and add custom order numbers (#500)

---
+++ packages/react-app-revamp/components/UI/Markdown/components/MarkdownImage/index.tsx
@@ -0,0 +1,30 @@
+/* eslint-disable @next/next/no-img-element */
+import { FC, useState } from "react";
+
+interface MarkdownImageProps {
+  imageSize: "compact" | "full";
+  src: string;
+}
+
+const MarkdownImage: FC<MarkdownImageProps> = ({ src, imageSize }) => {
+  const [error, setError] = useState(false);
+  const size = imageSize === "compact" ? "w-[170px]" : "w-[350px]";
+
+  if (!src) {
+    return <p>No image available</p>;
+  }
+
+  if (error) {
+    return (
+      <p>
+        <a href={src} target="_blank" rel="noopener noreferrer">
+          {src}
+        </a>
+      </p>
+    );
+  }
+
+  return <img src={src} className={`${size}`} alt="image" onError={() => setError(true)} />;
+};
+
+export default MarkdownImage;
