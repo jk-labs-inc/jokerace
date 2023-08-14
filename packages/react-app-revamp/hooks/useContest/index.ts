5ea8fb77a0bc8fccbd7f431cc51483564ebc3c50
zho <medenicablazo@gmail.com>
2023-08-14 21:18:11 +0200

    bug: reload user qualifications when account is changed (#514)

--- packages/react-app-revamp/hooks/useContest/index.ts
+++ packages/react-app-revamp/hooks/useContest/index.ts
@@ -14,7 +14,6 @@ import { FetchBalanceResult, readContract, readContracts } from "@wagmi/core";
 import { differenceInMilliseconds, differenceInMinutes, isBefore, minutesToMilliseconds } from "date-fns";
 import { BigNumber, utils } from "ethers";
 import { fetchFirstToken, fetchNativeBalance, fetchTokenBalances } from "lib/contests";
-import { generateMerkleTree, Recipient } from "lib/merkletree/generateMerkleTree";
 import { useRouter } from "next/router";
 import { useState } from "react";
 import { CustomError } from "types/error";
@@ -67,6 +66,7 @@ export function useContest() {
     setVotesClose,
     setVotesOpen,
     setRewards,
+    setSubmissionMerkleRoot,
     setSubmissionsOpen,
     setCanUpdateVotesInRealTime,
     setIsReadOnly,
@@ -144,7 +144,7 @@ export function useContest() {
       const submissionMerkleRoot = results[10].result as string;
       setContestName(results[0].result as string);
       setContestAuthor(results[1].result as string, results[1].result as string);
-
+      setSubmissionMerkleRoot(submissionMerkleRoot);
       setContestMaxNumberSubmissionsPerUser(contestMaxNumberSubmissionsPerUser);
       setContestMaxProposalCount(contestMaxProposalCount);
       setSubmissionsOpen(submissionsOpenDate);
