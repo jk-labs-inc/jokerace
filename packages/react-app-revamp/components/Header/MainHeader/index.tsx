d393e2bd5772160320e2cef8787055ffc6e33ebb
zho <medenicablazo@gmail.com>
2023-08-23 18:47:37 +0200

    enhancement: add user profile logo in header for profile and other stuff (#545)
    * enhancement: add user profile logo in header for profile and other stuff
    
    * refactor: remove margin when not connected ( quick fix )
    
    * refactor: change placement of profile logo a la sean

--- packages/react-app-revamp/components/Header/MainHeader/index.tsx
+++ packages/react-app-revamp/components/Header/MainHeader/index.tsx
@@ -1,16 +1,24 @@
-import { ROUTE_CREATE_CONTEST } from "@config/routes";
+/* eslint-disable @next/next/no-img-element */
+import EthereumAddress from "@components/UI/EtheuremAddress";
+import { ROUTE_CREATE_CONTEST, ROUTE_VIEW_CREATOR } from "@config/routes";
 import { HomeIcon } from "@heroicons/react/outline";
 import { ConnectButton, useAccountModal, useConnectModal } from "@rainbow-me/rainbowkit";
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 import { useMedia } from "react-use";
 import { useAccount } from "wagmi";
 
 const MainHeader = () => {
-  const { isConnected } = useAccount();
+  const { isConnected, address } = useAccount();
   const { openConnectModal } = useConnectModal();
   const { openAccountModal } = useAccountModal();
   const isMobileOrTablet = useMedia("(max-width: 1024px)");
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
 
   const lowerDeviceHeader = (
     <header className="flex flex-row items-center justify-between px-4 mt-4">
@@ -31,13 +39,20 @@ const MainHeader = () => {
         className="md:hidden transition-all duration-500"
       >
         {isConnected ? (
-          <Image width={30} height={30} src="/create-flow/wallet-connected.svg" alt="wallet-connected" />
+          <div className="flex gap-2">
+            {isClient && address && (
+              <Link href={`${ROUTE_VIEW_CREATOR}/${address}`}>
+                <EthereumAddress ethereumAddress={address} shortenOnFallback avatarVersion />
+              </Link>
+            )}
+            <Image width={30} height={30} src="/create-flow/wallet-connected.svg" alt="wallet-connected" />
+          </div>
         ) : (
           <Image width={30} height={30} src="/create-flow/wallet.svg" alt="wallet" />
         )}
       </div>
       <div className="hidden md:flex">
-        <ConnectButton showBalance={false} accountStatus="full" label="Connect wallet" />
+        <ConnectButton showBalance={false} accountStatus="address" label="Connect wallet" />
       </div>
     </header>
   );
@@ -52,7 +67,11 @@ const MainHeader = () => {
             <h1 className="font-sabo text-primary-10 text-[40px]">JOKERACE</h1>
           </Link>
           <div className="flex-1 flex justify-center items-center">
-            <div className="bg-true-black flex items-center gap-5 ml-[100px] text-[24px] font-bold border-2 rounded-[20px] py-[2px] px-[30px] border-primary-10 shadow-create-header">
+            <div
+              className={`bg-true-black flex items-center gap-5 ${
+                isConnected ? "ml-[100px]" : ""
+              }text-[24px] font-bold border-2 rounded-[20px] py-[2px] px-[30px] border-primary-10 shadow-create-header`}
+            >
               <Link href="/contests" className="text-primary-10">
                 play
               </Link>
@@ -62,8 +81,13 @@ const MainHeader = () => {
             </div>
           </div>
 
-          <div className="flex" style={{ minWidth: "260px" }}>
-            <ConnectButton showBalance={false} accountStatus="full" label="Connect wallet" />
+          <div className="flex gap-3 items-center" style={{ minWidth: "260pxs" }}>
+            {isClient && address ? (
+              <Link href={`${ROUTE_VIEW_CREATOR}/${address}`}>
+                <EthereumAddress ethereumAddress={address} shortenOnFallback avatarVersion />
+              </Link>
+            ) : null}
+            <ConnectButton showBalance={false} accountStatus="address" label="Connect wallet" />
           </div>
         </header>
       )}
--- packages/react-app-revamp/pages/index.tsx
+++ packages/react-app-revamp/pages/index.tsx
@@ -105,7 +105,7 @@ const Page: NextPage = props => {
             <Button intent={`${isConnected ? "primary" : "neutral-outline"}`} className="hidden xs:flex">
               Create contest
             </Button>
-            <ConnectButton showBalance={false} accountStatus="full" label="Connect wallet" />
+            <ConnectButton showBalance={false} accountStatus="address" label="Connect wallet" />
           </div>
         </div>
 
