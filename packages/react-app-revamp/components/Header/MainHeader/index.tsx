7fd01d9268a25de2c8e6373d069162aaf221030c
blazh <medenicablazo@gmail.com>
2023-08-23 18:26:29 +0200

    refactor: remove margin when not connected ( quick fix )

--- packages/react-app-revamp/components/Header/MainHeader/index.tsx
+++ packages/react-app-revamp/components/Header/MainHeader/index.tsx
@@ -67,7 +67,11 @@ const MainHeader = () => {
             <h1 className="font-sabo text-primary-10 text-[40px]">JOKERACE</h1>
           </Link>
           <div className="flex-1 flex justify-center items-center">
-            <div className="bg-true-black flex items-center gap-5 ml-[100px] text-[24px] font-bold border-2 rounded-[20px] py-[2px] px-[30px] border-primary-10 shadow-create-header">
+            <div
+              className={`bg-true-black flex items-center gap-5 ${
+                isConnected ? "ml-[100px]" : ""
+              }text-[24px] font-bold border-2 rounded-[20px] py-[2px] px-[30px] border-primary-10 shadow-create-header`}
+            >
               <Link href="/contests" className="text-primary-10">
                 play
               </Link>
