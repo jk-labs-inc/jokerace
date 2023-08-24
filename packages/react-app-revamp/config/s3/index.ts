034d4389aba213b14d24360a1ce9109a75d30250
blazh <medenicablazo@gmail.com>
2023-08-24 15:57:31 +0200

    chore: add env variables in s3 client initialization

--- packages/react-app-revamp/config/s3/index.ts
+++ packages/react-app-revamp/config/s3/index.ts
@@ -1,15 +1,10 @@
-import {
-  S3Client,
-  ListBucketsCommand,
-  ListObjectsV2Command,
-  GetObjectCommand,
-  PutObjectCommand,
-} from "@aws-sdk/client-s3";
+import { S3Client } from "@aws-sdk/client-s3";
 
 export const s3 = new S3Client({
   region: "auto",
+  endpoint: `https://${process.env.NEXT_PUBLIC_R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
   credentials: {
-    accessKeyId: process.env.R2_ACCESS_KEY_ID,
-    secretAccessKey: process.env.R2_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.NEXT_PUBLIC_R2_ACCESS_KEY_ID as string,
+    secretAccessKey: process.env.NEXT_PUBLIC_R2_SECRET_ACCESS_KEY as string,
   },
 });
