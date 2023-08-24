e35b5ee350b7b583275838029e351e5cecac2b17
blazh <medenicablazo@gmail.com>
2023-08-24 17:28:04 +0200

    chore: remove bucket name from endpoint as it is already prefixed

--- packages/react-app-revamp/config/s3/index.ts
+++ packages/react-app-revamp/config/s3/index.ts
@@ -2,7 +2,7 @@ import { S3Client } from "@aws-sdk/client-s3";
 
 export const s3 = new S3Client({
   region: "auto",
-  endpoint: `https://${process.env.NEXT_PUBLIC_R2_ACCOUNT_ID}.r2.cloudflarestorage.com/${process.env.NEXT_PUBLIC_MERKLE_TREES_BUCKET}}`,
+  endpoint: `https://${process.env.NEXT_PUBLIC_R2_ACCOUNT_ID}.r2.cloudflarestorage.com/`,
   credentials: {
     accessKeyId: process.env.NEXT_PUBLIC_R2_ACCESS_KEY_ID as string,
     secretAccessKey: process.env.NEXT_PUBLIC_R2_SECRET_ACCESS_KEY as string,
