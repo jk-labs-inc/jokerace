afac2482abc403cd6a4aa32716ef9a6fbf9dec8c
Sean McCaffery <sean.mccaffery9@gmail.com>
2023-08-28 17:19:43 -0400

    chain: add fuse

---
+++ packages/react-app-revamp/config/wagmi/custom-chains/fuse.ts
@@ -0,0 +1,24 @@
+import { Chain } from "@rainbow-me/rainbowkit";
+
+export const fuse: Chain = {
+  id: 122,
+  name: "fuse",
+  network: "fuse",
+  nativeCurrency: {
+    decimals: 18,
+    name: "FLUX",
+    symbol: "FLUX",
+  },
+  rpcUrls: {
+    public: {
+      http: ["https://rpc.fuse.io"],
+    },
+    default: {
+      http: ["https://rpc.fuse.io"],
+    },
+  },
+  blockExplorers: {
+    escan: { name: "Fuse Block Explorer", url: "https://explorer.fuse.io/" },
+    default: { name: "Fuse Block Explorer", url: "https://explorer.fuse.io/" },
+  },
+};
