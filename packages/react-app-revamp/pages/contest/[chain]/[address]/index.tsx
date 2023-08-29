91b9e111332872037905d8512158fb795ee0ba03
blazh <medenicablazo@gmail.com>
2023-08-29 12:49:36 +0200

    chore: trying out more settings from next/seo

--- packages/react-app-revamp/pages/contest/[chain]/[address]/index.tsx
+++ packages/react-app-revamp/pages/contest/[chain]/[address]/index.tsx
@@ -15,7 +15,34 @@ const Page: NextPage = (props: PageProps) => {
 
   return (
     <>
-      <NextSeo title="Simple Usage Example" description="A short description goes here." />
+      <NextSeo
+        title="Using More of Config"
+        description="This example uses more of the available config options."
+        openGraph={{
+          url: "https://www.url.ie/a",
+          title: "Open Graph Title",
+          description: "Open Graph Description",
+          images: [
+            {
+              url: "https://www.example.ie/og-image-01.jpg",
+              width: 800,
+              height: 600,
+              alt: "Og Image Alt",
+              type: "image/jpeg",
+            },
+            {
+              url: "https://www.example.ie/og-image-02.jpg",
+              width: 900,
+              height: 800,
+              alt: "Og Image Alt Second",
+              type: "image/jpeg",
+            },
+            { url: "https://www.example.ie/og-image-03.jpg" },
+            { url: "https://www.example.ie/og-image-04.jpg" },
+          ],
+          siteName: "SiteName",
+        }}
+      />
     </>
   );
 };
