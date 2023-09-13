3f9ececcbfc8d181382d658d5e9b41e15cbaac86
Sean McCaffery <sean.mccaffery9@gmail.com>
2023-09-13 17:51:15 -0400

    chore: use alchemy for base

--- packages/react-app-revamp/config/wagmi/custom-chains/base.ts
+++ packages/react-app-revamp/config/wagmi/custom-chains/base.ts
@@ -11,7 +11,7 @@ export const base: Chain = {
   },
   rpcUrls: {
     public: { http: ["https://mainnet.base.org"] },
-    default: { http: ["https://mainnet.base.org"] },
+    default: { http: [`https://base-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_KEY}`] },
   },
   blockExplorers: {
     etherscan: { name: "Base Mainnet Scan", url: "https://basescan.org" },
