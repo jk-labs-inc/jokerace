7efd8a2f172e919dbf652c1498b8fba4fde41696
Sean McCaffery <sean.mccaffery9@gmail.com>
2023-09-18 21:05:38 -0400

    enhancement: add participants table index (#675)

--- packages/react-app-revamp/supabase_schemas/contest_participants_v3.sql
+++ packages/react-app-revamp/supabase_schemas/contest_participants_v3.sql
@@ -8,3 +8,5 @@ create table
     network_name character varying null,
     constraint contest_participants_v3_pkey primary key (uuid)
   ) tablespace pg_default;
+
+create index if not exists idx_contest_participants_v3_network_name_contest_address_user_a on public.contest_participants_v3 using btree (network_name, contest_address, user_address) tablespace pg_default;
