b0bf6e8714510b979768fd6b3cce8def210699eb
Sean McCaffery <sean.mccaffery9@gmail.com>
2023-09-22 19:28:12 -0400

    chore: versioning

--- packages/forge/src/governance/Governor.sol
+++ packages/forge/src/governance/Governor.sol
@@ -71,7 +71,7 @@ abstract contract Governor is Context, ERC165, EIP712, GovernorMerkleVotes, IGov
      * @dev See {IGovernor-version}.
      */
     function version() public view virtual override returns (string memory) {
-        return "3.13";
+        return "3.14";
     }
 
     /**
