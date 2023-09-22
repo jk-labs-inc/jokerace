d15d375bfe61864b23787835b091898e9a2a3271
blazh <medenicablazo@gmail.com>
2023-09-22 15:59:35 +0200

    feat: add functionality to fetch data for submissions and voting activity

--- packages/react-app-revamp/lib/user/index.tsx
+++ packages/react-app-revamp/lib/user/index.tsx
@@ -1,60 +1,78 @@
 import { supabase } from "@config/supabase";
-import { isSupabaseConfigured } from "@helpers/database";
 
-const getPagination = (currentPage: number, itemsPerPage: number) => {
-  const from = (currentPage - 1) * itemsPerPage;
-  const to = from + itemsPerPage - 1;
-  return { from, to };
-};
-
-export async function getUserSubmittedProposals(userAddress: string, currentPage: number, itemsPerPage: number) {
-  if (isSupabaseConfigured) {
-    const { from, to } = getPagination(currentPage, itemsPerPage);
-    try {
-      const result = await supabase
-        .from("analytics_contest_participants_v3")
-        .select("network_name, contest_address, proposal_id, created_at", { count: "exact" })
-        .eq("user_address", userAddress)
-        .is("vote_amount", null)
-        .range(from, to);
-
-      const { data, count, error } = result;
-      if (error) {
-        throw new Error(error.message);
-      }
-
-      return { data, count };
-    } catch (e) {
-      console.error(e);
-    }
-  }
-  return { data: [], count: 0 };
+interface ProposalCriteria {
+  user_address: string;
+  [key: string]: any;
 }
 
-export async function getUserVotedProposals(userAddress: string, currentPage: number, itemsPerPage: number) {
-  if (isSupabaseConfigured) {
-    const { from, to } = getPagination(currentPage, itemsPerPage);
-    try {
-      const result = await supabase
-        .from("analytics_contest_participants_v3")
-        .select("network_name, contest_address, proposal_id, created_at", { count: "exact" })
-        .eq("user_address", userAddress)
-        .not("vote_amount", "eq", null)
-        .order("proposal_id", { ascending: true })
-        .order("contest_address", { ascending: true })
-        .order("network_name", { ascending: true })
-        .order("created_at", { ascending: true })
-        .range(from, to);
+async function fetchProposals(criteria: ProposalCriteria, range: { from: number; to: number }) {
+  const result = await supabase
+    .from("analytics_contest_participants_v3")
+    .select("network_name, contest_address, proposal_id, created_at")
+    .match(criteria)
+    .range(range.from, range.to);
 
-      const { data, count, error } = result;
-      if (error) {
-        throw new Error(error.message);
-      }
-
-      return { data, count };
-    } catch (e) {
-      console.error(e);
-    }
+  const { data, error } = result;
+  if (error) {
+    console.error(error);
+    return [];
   }
-  return { data: [], count: 0 };
+
+  return data;
+}
+
+async function getContestDetailsByAddresses(contestAddresses: string[]) {
+  const result = await supabase.from("contests_v3").select("title").in("address", contestAddresses);
+
+  const { data, error } = result;
+  if (error) {
+    console.error(error);
+    return [];
+  }
+
+  return data;
+}
+
+function getPagination(currentPage: number, itemsPerPage: number) {
+  const from = (currentPage - 1) * itemsPerPage;
+  const to = currentPage * itemsPerPage - 1;
+  return { from, to };
+}
+
+// Merge proposals with their respective contest details
+function mergeProposalsWithContests(proposals: any[], contests: any[]): any[] {
+  return proposals.map(proposal => {
+    const matchedContest = contests.find(contest => contest.address === proposal.contest_address);
+    return {
+      ...proposal,
+      contestDetails: matchedContest,
+    };
+  });
+}
+
+export async function fetchUserSubmissions(
+  userAddress: string,
+  currentPage: number,
+  itemsPerPage: number,
+): Promise<any[]> {
+  const range = getPagination(currentPage, itemsPerPage);
+  const criteria: ProposalCriteria = { user_address: userAddress, vote_amount: null };
+
+  const proposals = await fetchProposals(criteria, range);
+  const contestAddresses = proposals.map(p => p.contest_address);
+  const contests = await getContestDetailsByAddresses(contestAddresses);
+
+  return mergeProposalsWithContests(proposals, contests);
+}
+
+export async function fetchUserVotes(userAddress: string, currentPage: number, itemsPerPage: number): Promise<any[]> {
+  const range = getPagination(currentPage, itemsPerPage);
+  const criteria: ProposalCriteria = { user_address: userAddress };
+  criteria["vote_amount"] = { neq: null };
+
+  const proposals = await fetchProposals(criteria, range);
+  const contestAddresses = proposals.map(p => p.contest_address);
+  const contests = await getContestDetailsByAddresses(contestAddresses);
+
+  return mergeProposalsWithContests(proposals, contests);
 }
