58844eb7d26b744125d7008181d7b36a979e59f8
Sean McCaffery <sean.mccaffery9@gmail.com>
2023-09-23 18:00:26 -0400

    chore: add in RB Tree

---
+++ packages/forge/src/utils/TestBokkyPooBahsRedBlackTreeRaw.sol
@@ -0,0 +1,56 @@
+pragma solidity ^0.8.0;
+
+import "BokkyPooBahsRedBlackTreeLibrary.sol";
+
+// ----------------------------------------------------------------------------
+// BokkyPooBah's Red-Black Tree Library v1.0-pre-release-a - Contract for testing
+//
+// A Solidity Red-Black Tree binary search library to store and access a sorted
+// list of unsigned integer data. The Red-Black algorithm rebalances the binary
+// search tree, resulting in O(log n) insert, remove and search time (and ~gas)
+//
+// https://github.com/bokkypoobah/BokkyPooBahsRedBlackTreeLibrary
+//
+//
+// Enjoy. (c) BokkyPooBah / Bok Consulting Pty Ltd 2020. The MIT Licence.
+// ----------------------------------------------------------------------------
+contract TestBokkyPooBahsRedBlackTreeRaw {
+    using BokkyPooBahsRedBlackTreeLibrary for BokkyPooBahsRedBlackTreeLibrary.Tree;
+
+    BokkyPooBahsRedBlackTreeLibrary.Tree tree;
+
+    event Log(string where, uint key, uint value);
+
+    constructor() public {
+    }
+    function root() public view returns (uint _key) {
+        _key = tree.root;
+    }
+    function first() public view returns (uint _key) {
+        _key = tree.first();
+    }
+    function last() public view returns (uint _key) {
+        _key = tree.last();
+    }
+    function next(uint key) public view returns (uint _key) {
+        _key = tree.next(key);
+    }
+    function prev(uint key) public view returns (uint _key) {
+        _key = tree.prev(key);
+    }
+    function exists(uint key) public view returns (bool _exists) {
+        _exists = tree.exists(key);
+    }
+    function getNode(uint _key) public view returns (uint key, uint parent, uint left, uint right, bool red) {
+        (key, parent, left, right, red) = tree.getNode(_key);
+    }
+
+    function insert(uint _key) public {
+        tree.insert(_key);
+        // emit Log("insert", _key, 0);
+    }
+    function remove(uint _key) public {
+        tree.remove(_key);
+        // emit Log("remove", _key, 0);
+    }
+}
