467ef220b21b8ea1eb00df05e661fb814bf57545
Sean McCaffery <sean.mccaffery9@gmail.com>
2023-09-25 14:15:55 -0400

    test: have pragma so compilers not mad

--- packages/forge/test/RewardsModule.t.sol
+++ packages/forge/test/RewardsModule.t.sol
@@ -1,5 +1,5 @@
-// // SPDX-License-Identifier: UNLICENSED
-// pragma solidity ^0.8.13;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
 
 // import "@forge-std/Test.sol";
 // import "@openzeppelin/token/ERC20/presets/ERC20PresetFixedSupply.sol";
