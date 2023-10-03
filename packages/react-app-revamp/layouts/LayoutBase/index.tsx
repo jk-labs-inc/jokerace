8f1de9ce53a3e525d8e3561b7189450680a5261b
blazh <medenicablazo@gmail.com>
2023-10-03 13:01:28 +0200

    enhancement: remove footer on mobile

--- packages/react-app-revamp/layouts/LayoutBase/index.tsx
+++ packages/react-app-revamp/layouts/LayoutBase/index.tsx
@@ -1,6 +1,7 @@
 import Header from "@components/Header";
 import { FOOTER_LINKS } from "@config/links";
-import { useRouter } from "next/router";
+import { MediaQuery } from "@helpers/mediaQuery";
+import { useMediaQuery } from "react-responsive";
 
 interface LayoutBaseProps {
   children: React.ReactNode;
@@ -10,14 +11,13 @@ const LayoutBase = (props: LayoutBaseProps) => {
   const { children } = props;
   const allowedLinks = ["Github", "Mirror", "Uniswap", "OpenSea", "Twitter", "Uniswap", "Report a bug"];
   const filteredLinks = FOOTER_LINKS.filter(link => allowedLinks.includes(link.label));
-  const router = useRouter();
-  const createRoute = router.pathname === "/contest/new";
+  const isMobile = useMediaQuery({ maxWidth: 768 });
 
   return (
     <>
       <Header />
-      <main className="flex flex-col grow">{children}</main>
-      {!createRoute && (
+      <main className={`flex flex-col grow ${isMobile ? "pb-20" : ""}`}>{children}</main>
+      <MediaQuery minWidth={768}>
         <footer className="mt-auto py-20 xs:pb-0 xs:pt-32">
           <div className="text-neutral-11 container justify-center items-start text-[16px] flex flex-col pl-8 pr-8 md:pl-16 md:pr-16 space-y-1 xs:space-y-0 xs:space-i-4 xs:flex-row xs:flex-wrap mx-auto">
             {filteredLinks.map((link, key) => (
@@ -33,7 +33,7 @@ const LayoutBase = (props: LayoutBaseProps) => {
             ))}
           </div>
         </footer>
-      )}
+      </MediaQuery>
     </>
   );
 };
