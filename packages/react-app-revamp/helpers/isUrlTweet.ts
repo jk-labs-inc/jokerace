e777ae8b1a2af0c045f62d51076771dae01d30f8
zho <medenicablazo@gmail.com>
2023-10-17 16:09:34 +0200

    bugfix: update regex to match whole html for tweet (#821)

--- packages/react-app-revamp/helpers/isUrlTweet.ts
+++ packages/react-app-revamp/helpers/isUrlTweet.ts
@@ -1,7 +1,18 @@
-export function isUrlTweet(str: string) {
+export function containsTweetUrl(str: string) {
   return (
-    str.match(
-      /^https?:\/\/((twitter\.com\/(?:#!\/)?(\w+)\/status(es)?\/(\d+))|(x\.com\/\w+\/status\/\d+))(\?\S+)?$/,
-    ) !== null
+    str.match(/https?:\/\/((twitter\.com\/(?:#!\/)?(\w+)\/status(es)?\/(\d+))|(x\.com\/\w+\/status\/\d+))(\?\S+)?/) !==
+    null
   );
+}
+
+export function extractTwitterUrlFromHTML(html: string): string | null {
+  const doc = new DOMParser().parseFromString(html, "text/html");
+
+  const textContent = doc.body.textContent || "";
+
+  const match = textContent.match(
+    /https?:\/\/((twitter\.com\/(?:#!\/)?(\w+)\/status(es)?\/(\d+))|(x\.com\/\w+\/status\/\d+))(\?\S+)?/,
+  );
+
+  return match ? match[0] : null;
 }
