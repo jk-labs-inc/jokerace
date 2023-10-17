2cadc8d3f4bfeb3933b4794ac4df1441335e4d26
Sean McCaffery <sean.mccaffery9@gmail.com>
2023-10-17 08:10:14 -0400

    chore: add mainnet ENS resolver config

--- packages/react-app-revamp/config/wagmi/custom-chains/mainnet.ts
+++ packages/react-app-revamp/config/wagmi/custom-chains/mainnet.ts
@@ -22,4 +22,17 @@ export const mainnet: Chain = {
     etherscan: { name: "Etherscan", url: "https://etherscan.io/" },
     default: { name: "Etherscan", url: "https://etherscan.io/" },
   },
+  contracts: {
+    ensRegistry: {
+      address: "0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e"
+    },
+    ensUniversalResolver: {
+      address: "0xc0497E381f536Be9ce14B0dD3817cBcAe57d2F62",
+      blockCreated: 16966585
+    },
+    multicall3: {
+      address: "0xca11bde05977b3631167028862be2a173976ca11",
+      blockCreated: 14353601
+    }
+  }
 };
