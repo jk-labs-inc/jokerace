cc8df87394425138ebeaf8f94040b309e58dfd3a
blazh <medenicablazo@gmail.com>
2023-10-31 10:34:40 +0100

    bugfix: remove retrieving proposalId logic

--- packages/react-app-revamp/hooks/useSubmitProposal/index.ts
+++ packages/react-app-revamp/hooks/useSubmitProposal/index.ts
@@ -3,7 +3,6 @@ import { chains } from "@config/wagmi";
 import { TransactionResponse } from "@ethersproject/abstract-provider";
 import { extractPathSegments } from "@helpers/extractPath";
 import getContestContractVersion from "@helpers/getContestContractVersion";
-import { getProposalId } from "@helpers/getProposalId";
 import { useContestStore } from "@hooks/useContest/store";
 import { useError } from "@hooks/useError";
 import { useGenerateProof } from "@hooks/useGenerateProof";
@@ -103,7 +102,7 @@ export function useSubmitProposal() {
           hash,
         });
 
-        const proposalId = await getProposalId(proposalCore, contractConfig);
+        const proposalId = getProposalIdFromReceipt(receipt, abi);
 
         setTransactionData({
           chainId: chain?.id,
@@ -133,6 +132,16 @@ export function useSubmitProposal() {
         setIsLoading(false);
       }
     });
+  }
+
+  function getProposalIdFromReceipt(receipt: TransactionReceipt, abi: any): string {
+    const iface = new utils.Interface(abi);
+    const log = receipt.logs[0];
+    const event = iface.parseLog(log);
+
+    const proposalIdDecimal = BigNumber.from(event.args.proposalId).toString();
+
+    return proposalIdDecimal;
   }
 
   return {
