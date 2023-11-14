c23732095bdcf5862007697a52c1dcc8d3417659
Sean McCaffery <sean.mccaffery9@gmail.com>
2023-11-14 15:58:54 -0500

    chain: add manta (#982)
    * chain: add manta
    
    * chore: update name to mantaPacific
    
    * chore: add mantaPacific svg

---
+++ packages/react-app-revamp/config/wagmi/custom-chains/mantaPacific.ts
@@ -0,0 +1,25 @@
+import { Chain } from "@rainbow-me/rainbowkit";
+
+export const mantaPacific: Chain = {
+  id: 169,
+  name: "mantaPacific",
+  network: "mantaPacific",
+  iconUrl: "/mantaPacific.svg",
+  nativeCurrency: {
+    decimals: 18,
+    name: "ETH",
+    symbol: "ETH",
+  },
+  rpcUrls: {
+    public: {
+      http: ["https://pacific-rpc.manta.network/http"],
+    },
+    default: {
+      http: ["https://pacific-rpc.manta.network/http"],
+    },
+  },
+  blockExplorers: {
+    etherscan: { name: "Manta Pacific Etherscan", url: "https://pacific-explorer.manta.network" },
+    default: { name: "Manta Pacific Etherscan", url: "https://pacific-explorer.manta.network" },
+  },
+};
