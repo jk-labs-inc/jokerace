76ee415c6b179a330c1bc1af7e83a58df3345a91
Sean McCaffery <sean.mccaffery9@gmail.com>
2023-11-16 23:24:40 -0500

    chore: rm unused Context import and redundant IGovernor import from RewardsModule

--- packages/forge/src/modules/RewardsModule.sol
+++ packages/forge/src/modules/RewardsModule.sol
@@ -4,8 +4,6 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/utils/Address.sol";
-import "@openzeppelin/utils/Context.sol";
-import "../governance/IGovernor.sol";
 import "../governance/extensions/GovernorCountingSimple.sol";
 
 /**
@@ -26,7 +24,7 @@ import "../governance/extensions/GovernorCountingSimple.sol";
  * tokens that apply fees during transfers, are likely to not be supported as expected. If in doubt, we encourage you
  * to run tests before sending real value to this contract.
  */
-contract RewardsModule is Context {
+contract RewardsModule {
     event PayeeAdded(uint256 ranking, uint256 shares);
     event PaymentReleased(address to, uint256 amount);
     event ERC20PaymentReleased(IERC20 indexed token, address to, uint256 amount);
