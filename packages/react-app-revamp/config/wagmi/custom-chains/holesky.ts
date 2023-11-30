1456e874ce7dc5ec3ed43fca565dc569e7fa00d1
Sean McCaffery <sean.mccaffery9@gmail.com>
2023-11-30 16:32:14 -0500

    chain: add holesky and redstoneHolesky (#1063)

---
+++ packages/react-app-revamp/config/wagmi/custom-chains/holesky.ts
@@ -0,0 +1,24 @@
+import { Chain } from "@rainbow-me/rainbowkit";
+
+export const holesky: Chain = {
+  id: 17000,
+  name: "holesky",
+  network: "holesky",
+  nativeCurrency: {
+    decimals: 18,
+    name: "Ether",
+    symbol: "ETH",
+  },
+  rpcUrls: {
+    public: {
+      http: ["https://ethereum-holesky.publicnode.com"],
+    },
+    default: {
+      http: ["https://ethereum-holesky.publicnode.com"],
+    },
+  },
+  blockExplorers: {
+    etherscan: { name: "Holesky Etherscan", url: "https://holesky.etherscan.io/" },
+    default: { name: "Holesky Etherscan", url: "https://holesky.etherscan.io/" },
+  },
+};
