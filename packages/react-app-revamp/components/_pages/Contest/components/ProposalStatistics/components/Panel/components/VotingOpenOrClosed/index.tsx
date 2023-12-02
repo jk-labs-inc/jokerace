c3cde7e4c44f147e024831d3633c79887a9ca289
zho <medenicablazo@gmail.com>
2023-12-02 10:25:01 +0100

    chore: apply style for total votes and votes cast on mobile (#1073)

--- packages/react-app-revamp/components/_pages/Contest/components/ProposalStatistics/components/Panel/components/VotingOpenOrClosed/index.tsx
+++ packages/react-app-revamp/components/_pages/Contest/components/ProposalStatistics/components/Panel/components/VotingOpenOrClosed/index.tsx
@@ -73,11 +73,11 @@ const ProposalStatisticsPanelVotingOpenOrClosed: FC<ProposalStatisticsPanelVotin
   };
 
   return (
-    <div className="flex gap-1 items-center">
+    <div className="flex flex-col md:flex-row gap-1 md:items-center">
       <p className="text-[16px] text-neutral-11">
         {submissionsCount} submission{submissionsCount > 1 ? "s" : ""}
       </p>
-      &#8226;
+      <span className="hidden md:block">&#8226;</span>
       <div className="flex gap-1 items-center text-[16px] text-neutral-11">
         {renderTotalVotesCast()} out of {renderTotalVotes()} votes deployed in contest
       </div>
