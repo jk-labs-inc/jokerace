2705b304dca7941f9860f451f2a71e5a4b33bd5f
Sean McCaffery <sean.mccaffery9@gmail.com>
2023-12-05 14:34:59 -0500

    chore: version and bytecode

--- packages/forge/src/governance/Governor.sol
+++ packages/forge/src/governance/Governor.sol
@@ -134,7 +134,7 @@ abstract contract Governor is GovernorSorting, GovernorMerkleVotes {
     }
 
     function version() public pure returns (string memory) {
-        return "4.11";
+        return "4.12";
     }
 
     function hashProposal(ProposalCore memory proposal) public pure returns (uint256) {
--- packages/react-app-revamp/helpers/getRewardsModuleContractVersion.ts
+++ packages/react-app-revamp/helpers/getRewardsModuleContractVersion.ts
@@ -31,6 +31,7 @@ import DeleteInMapAfterForLoopRewards from "@contracts/bytecodeAndAbi/modules/Re
 import AddGetPropIdsWithForVotesRewards from "@contracts/bytecodeAndAbi/modules/RewardsModule.4.9.addGetPropIdsWithForVotes.sol/RewardsModule.json";
 import RmImmutableKeywordRewards from "@contracts/bytecodeAndAbi/modules/RewardsModule.4.10.rmImmutableKeyword.sol/RewardsModule.json";
 import GasOptimizeGettersRewards from "@contracts/bytecodeAndAbi/modules/RewardsModule.4.11.gasOptimizeGetters.sol/RewardsModule.json";
+import AllowCancelCompletedRewards from "@contracts/bytecodeAndAbi/modules/RewardsModule.4.12.allowCancelCompleted.sol/RewardsModule.json";
 import DeployedRewardsContract from "@contracts/bytecodeAndAbi/modules/RewardsModule.sol/RewardsModule.json";
 import { ethers } from "ethers";
 import { getEthersProvider } from "./ethers";
@@ -43,7 +44,9 @@ export async function getRewardsModuleContractVersion(address: string, chainId: 
   try {
     const version: string = await executeWithTimeout(MAX_TIME_TO_WAIT_FOR_RPC, contract.version());
 
-    if (version === "4.11") {
+    if (version === "4.12") {
+      return GasOptimizeGettersRewards.abi;
+    } else if (version === "4.11") {
       return GasOptimizeGettersRewards.abi;
     } else if (version === "4.10") {
       return RmImmutableKeywordRewards.abi;
