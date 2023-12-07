24200eaee178cd72de76855846294b3cc5e8b92c
blazh <medenicablazo@gmail.com>
2023-12-07 08:37:32 +0100

    bugfix: log abi and version

--- packages/react-app-revamp/lib/proposal/index.ts
+++ packages/react-app-revamp/lib/proposal/index.ts
@@ -54,6 +54,8 @@ export const fetchProposalData = async (
   try {
     const { abi, version } = await getContestContractVersion(address, chainId);
 
+    console.log("abi", abi, "version", version);
+
     if (!abi) return null;
 
     let contracts = [
