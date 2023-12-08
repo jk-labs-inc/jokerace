e8b69ee15513369240f5ce883aed028c5022a535
blazh <medenicablazo@gmail.com>
2023-12-08 09:48:46 +0100

    enhancement: apply styles to mobile comment form

--- packages/react-app-revamp/components/Comments/components/Form/components/Input/index.tsx
+++ packages/react-app-revamp/components/Comments/components/Form/components/Input/index.tsx
@@ -1,5 +1,4 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import ButtonV3, { ButtonSize } from "@components/UI/ButtonV3";
 import EthereumAddress from "@components/UI/EtheuremAddress";
 import { DisableEnter, ShiftEnterCreateExtension } from "@helpers/editor";
 import { useConnectModal } from "@rainbow-me/rainbowkit";
@@ -8,10 +7,10 @@ import Placeholder from "@tiptap/extension-placeholder";
 import { EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import { switchNetwork } from "@wagmi/core";
-import Image from "next/image";
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useMedia } from "react-use";
 import { useAccount, useNetwork } from "wagmi";
+import CommentFormInputSubmitButton from "./components/SubmitButton";
 
 interface CommentsFormInputProps {
   contestChainId: number;
@@ -56,7 +55,7 @@ const CommentsFormInput: React.FC<CommentsFormInputProps> = ({ onSend, contestCh
         setContainerHeight(containerRef.current.clientHeight);
       }
 
-      if (editor.getText().length > 0 && isConnected && isUserOnCorrectNetwork) {
+      if (editor.getText().length > 0) {
         setAllowSend(true);
       } else {
         setAllowSend(false);
@@ -85,7 +84,7 @@ const CommentsFormInput: React.FC<CommentsFormInputProps> = ({ onSend, contestCh
   };
 
   const handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
-    if (event.key === "Enter" && !event.shiftKey) {
+    if (event.key === "Enter" && !event.shiftKey && !isMobile) {
       onSendCommentHandler();
     }
   };
@@ -109,26 +108,16 @@ const CommentsFormInput: React.FC<CommentsFormInputProps> = ({ onSend, contestCh
         className="bg-transparent outline-none w-full md:w-[660px] overflow-y-auto max-h-[300px] "
         onKeyDown={handleKeyDown}
       />
-      {!isConnected ? (
-        <ButtonV3 colorClass="bg-gradient-vote rounded-[40px]" size={ButtonSize.SMALL_LONG} onClick={openConnectModal}>
-          {isMobile ? "connect" : "connect wallet"}
-        </ButtonV3>
-      ) : !isUserOnCorrectNetwork ? (
-        <ButtonV3 colorClass="bg-gradient-create rounded-[40px]" size={ButtonSize.SMALL_LONG} onClick={onSwitchNetwork}>
-          {isMobile ? "switch chain" : "switch network"}
-        </ButtonV3>
-      ) : (
-        <Image
-          className={`ml-auto pr-1 ${allowSend ? "filter-send-commment-icon" : ""} ${
-            isAdding ? "filter-send-commment-icon opacity-50 pointer-events-none" : "cursor-pointer"
-          }`}
-          src="/comments/send.svg"
-          alt="send"
-          width={32}
-          height={32}
-          onClick={onSendCommentHandler}
-        />
-      )}
+      <CommentFormInputSubmitButton
+        allowSend={allowSend}
+        isAdding={isAdding}
+        isMobile={isMobile}
+        isConnected={isConnected}
+        isUserOnCorrectNetwork={isUserOnCorrectNetwork}
+        onSend={onSendCommentHandler}
+        onConnect={openConnectModal}
+        onSwitchNetwork={onSwitchNetwork}
+      />
     </div>
   );
 };
