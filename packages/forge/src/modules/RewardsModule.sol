6c4cd99c01eea9e1a1c7748db50b764bbbe91acf
Sean McCaffery <sean.mccaffery9@gmail.com>
2023-12-10 12:15:28 -0500

    chore: pin all pragma to 0.8.19

--- packages/forge/src/modules/RewardsModule.sol
+++ packages/forge/src/modules/RewardsModule.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.19;
 
 import "@openzeppelin/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/utils/Address.sol";
