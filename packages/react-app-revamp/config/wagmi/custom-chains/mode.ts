004a1e0f6b51d9750110f768741e5d716df8818e
zho <medenicablazo@gmail.com>
2023-12-12 15:40:55 +0100

    chain: add mode mainnet (#1144)
    * chain: add mode dev mainnet
    
    * chore: reorg added chains
    
    ---------
    
    Co-authored-by: Sean McCaffery <sean.mccaffery9@gmail.com>

---
+++ packages/react-app-revamp/config/wagmi/custom-chains/mode.ts
@@ -0,0 +1,25 @@
+import { Chain } from "@rainbow-me/rainbowkit";
+
+export const mode: Chain = {
+  id: 34443,
+  name: "mode",
+  network: "mode",
+  iconUrl: "/mode.svg",
+  nativeCurrency: {
+    decimals: 18,
+    name: "Ether",
+    symbol: "ETH",
+  },
+  rpcUrls: {
+    public: {
+      http: ["https://mainnet.mode.network/"],
+    },
+    default: {
+      http: ["https://mainnet.mode.network/"],
+    },
+  },
+  blockExplorers: {
+    etherscan: { name: "Mode Mainnet Scan ", url: "https://explorer.mode.network/" },
+    default: { name: "Mode Mainnet Scan", url: "https://explorer.mode.network/" },
+  },
+};
