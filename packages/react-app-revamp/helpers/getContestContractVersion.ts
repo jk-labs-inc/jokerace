54d3329bda5ab0ec5a90d5c82aff788d893ff77e
Sean McCaffery <sean.mccaffery9@gmail.com>
2023-12-14 07:13:48 -0500

    feat: add emergency funcs (#1172)

--- packages/react-app-revamp/helpers/getContestContractVersion.ts
+++ packages/react-app-revamp/helpers/getContestContractVersion.ts
@@ -42,6 +42,7 @@ import RmShadowingPropIdsContract from "@contracts/bytecodeAndAbi/Contest.4.14.r
 import RmUnusedViewFuncContract from "@contracts/bytecodeAndAbi/Contest.4.15.rmUnusedViewFunc.sol/Contest.json";
 import PinAllToSameContract from "@contracts/bytecodeAndAbi/Contest.4.16.pinAllToSame.sol/Contest.json";
 import MitToAGPLContract from "@contracts/bytecodeAndAbi/Contest.4.17.mitToAGPL.sol/Contest.json";
+import AddEmergencyFuncsContract from "@contracts/bytecodeAndAbi/Contest.4.18.addEmergencyFuncs.sol/Contest.json";
 import DeployedContestContract from "@contracts/bytecodeAndAbi/Contest.sol/Contest.json";
 import { ethers, utils } from "ethers";
 import { getEthersProvider } from "./ethers";
@@ -57,7 +58,9 @@ export async function getContestContractVersion(address: string, chainId: number
 
     const defaultReturn = { abi: null, version: "unknown" };
 
-    if (version === "4.17") {
+    if (version === "4.18") {
+      return { abi: AddEmergencyFuncsContract.abi, version};
+    } else if (version === "4.17") {
       return { abi: MitToAGPLContract.abi, version};
     } else if (version === "4.16") {
       return { abi: PinAllToSameContract.abi, version};
