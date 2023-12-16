e9c7d15b623e53b79e0990fd318566f2d5a89483
Sean McCaffery <sean.mccaffery9@gmail.com>
2023-12-16 15:13:14 -0500

    chain: add neon and neonDevnet (#1202)

---
+++ packages/react-app-revamp/config/wagmi/custom-chains/neonDevnet.ts
@@ -0,0 +1,24 @@
+import { Chain } from "@rainbow-me/rainbowkit";
+
+export const neonDevnet: Chain = {
+  id: 245022926,
+  name: "neonDevnet",
+  network: "neonDevnet",
+  nativeCurrency: {
+    decimals: 18,
+    name: "NEON",
+    symbol: "NEON",
+  },
+  rpcUrls: {
+    public: {
+      http: ["https://devnet.neonevm.org"],
+    },
+    default: {
+      http: ["https://devnet.neonevm.org"],
+    },
+  },
+  blockExplorers: {
+    etherscan: { name: "Neon Devnet Scan", url: "https://devnet.neonscan.org/" },
+    default: { name: "Neon Devnet Scan", url: "https://devnet.neonscan.org/" },
+  },
+};
