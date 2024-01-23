ed1e90de2cd3ce3c059002c8dba4c85d0a26cc67
Sean McCaffery <sean.mccaffery9@gmail.com>
2024-01-22 21:27:46 -0500

    feat: add cost to vote and refactor to avoid stack too deep in constructor

--- packages/forge/src/Contest.sol
+++ packages/forge/src/Contest.sol
@@ -13,25 +13,9 @@ contract Contest is GovernorCountingSimple, GovernorModuleRegistry, GovernorEnga
         string memory _prompt,
         bytes32 _submissionMerkleRoot,
         bytes32 _votingMerkleRoot,
-        uint256[] memory _constructorIntParams
+        ConstructorIntArgs memory _constructorIntArgs
     )
-        Governor(
-            _name,
-            _prompt,
-            _submissionMerkleRoot,
-            _votingMerkleRoot,
-            _constructorIntParams[0], // contestStart
-            _constructorIntParams[1], // votingDelay,
-            _constructorIntParams[2], // votingPeriod,
-            _constructorIntParams[3], // numAllowedProposalSubmissions,
-            _constructorIntParams[4], // maxProposalCount
-            _constructorIntParams[5], // downvotingAllowed
-            _constructorIntParams[6], // costToPropose
-            _constructorIntParams[7] // percentageToCreator
-        )
-        GovernorSorting(
-            _constructorIntParams[8], // sortingEnabled
-            _constructorIntParams[9] // rankLimit
-        )
+        Governor(_name, _prompt, _submissionMerkleRoot, _votingMerkleRoot, _constructorIntArgs)
+        GovernorSorting(_constructorIntArgs.sortingEnabled, _constructorIntArgs.rankLimit)
     {}
 }
