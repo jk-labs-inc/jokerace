a48a2c2092576e8906cfd77bb4c2f2bae8249763
blazh <medenicablazo@gmail.com>
2024-02-01 09:00:29 +0100

    feat: update schemas

--- packages/react-app-revamp/supabase_schemas/chain_params.sql
+++ packages/react-app-revamp/supabase_schemas/chain_params.sql
@@ -2,5 +2,6 @@ create table
   public.chain_params (
     uuid uuid not null default gen_random_uuid (),
     network_name character varying not null,
-    min_cost_to_propose numeric not null 
+    min_cost_to_propose numeric not null,
+    min_cost_to_vote numeric not null
 ) tablespace pg_default;
