580abfa6d89a668e23d01f41a9623af52f82e394
Sean McCaffery <sean.mccaffery9@gmail.com>
2024-02-15 16:33:54 -0500

    chain: add syndicate frame (#1340)

---
+++ packages/react-app-revamp/config/wagmi/custom-chains/syndicateFrame.ts
@@ -0,0 +1,25 @@
+import { Chain } from "@rainbow-me/rainbowkit";
+
+export const syndicateFrame: Chain = {
+  id: 5101,
+  name: "syndicateFrame",
+  network: "syndicateFrame",
+  iconUrl: "/contest/mona-lisa-moustache.png",
+  nativeCurrency: {
+    decimals: 18,
+    name: "ETH",
+    symbol: "ETH",
+  },
+  rpcUrls: {
+    public: {
+      http: ["https://rpc-frame.syndicate.io"],
+    },
+    default: {
+      http: ["https://rpc-frame.syndicate.io"],
+    },
+  },
+  blockExplorers: {
+    etherscan: { name: "Syndicate Frame Block Explorer", url: "https://explorer-frame.syndicate.io/" },
+    default: { name: "Syndicate Frame Block Explorer", url: "https://explorer-frame.syndicate.io/" },
+  },
+};
