61627fa711c2fbcb3b04cd673eb40a961f6f195f
Sean McCaffery <sean.mccaffery9@gmail.com>
2024-02-14 19:01:57 -0500

    chain: add aevo (#1331)
    * chain: add aevo
    
    * chain: add logo
    
    ---------
    
    Co-authored-by: blazh <medenicablazo@gmail.com>

---
+++ packages/react-app-revamp/config/wagmi/custom-chains/aevo.ts
@@ -0,0 +1,25 @@
+import { Chain } from "@rainbow-me/rainbowkit";
+
+export const aevo: Chain = {
+  id: 2999,
+  name: "aevo",
+  network: "aevo",
+  iconUrl: "/aevo.svg",
+  nativeCurrency: {
+    decimals: 18,
+    name: "ETH",
+    symbol: "ETH",
+  },
+  rpcUrls: {
+    public: {
+      http: ["https://l2-aevo-mainnet-prod-0.t.conduit.xyz"],
+    },
+    default: {
+      http: ["https://l2-aevo-mainnet-prod-0.t.conduit.xyz"],
+    },
+  },
+  blockExplorers: {
+    etherscan: { name: "Aevo Mainnet Etherscan", url: "https://explorerl2new-aevo-mainnet-prod-0.t.conduit.xyz/" },
+    default: { name: "Aevo Mainnet Etherscan", url: "https://explorerl2new-aevo-mainnet-prod-0.t.conduit.xyz/" },
+  },
+};
