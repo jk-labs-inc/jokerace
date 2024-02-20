17931a2e34280588ba6fecdda8108b7c3abc407f
blazh <medenicablazo@gmail.com>
2024-02-20 15:08:22 +0100

    chore: remove next.config addition

--- packages/react-app-revamp/next.config.js
+++ packages/react-app-revamp/next.config.js
@@ -10,7 +10,6 @@ const nextConfig = {
   },
   webpack: config => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
-    config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
   },
   images: {
