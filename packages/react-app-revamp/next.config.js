cceac8a933177a00ca22b2ee6badc7c6941a3e8a
blazh <medenicablazo@gmail.com>
2024-02-20 15:02:43 +0100

    chore: next.config changes

--- packages/react-app-revamp/next.config.js
+++ packages/react-app-revamp/next.config.js
@@ -10,6 +10,7 @@ const nextConfig = {
   },
   webpack: config => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
+    config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
   },
   images: {
