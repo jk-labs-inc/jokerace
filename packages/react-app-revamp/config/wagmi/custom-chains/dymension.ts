476b7ec5de9bf8b2a3b67d684def9677c917a174
Sean McCaffery <sean.mccaffery9@gmail.com>
2024-03-01 22:37:23 -0500

    chain: add blast and dymension pt 2

---
+++ packages/react-app-revamp/config/wagmi/custom-chains/dymension.ts
@@ -0,0 +1,24 @@
+import { Chain } from "@rainbow-me/rainbowkit";
+
+export const dymension: Chain = {
+  id: 1100,
+  name: "dymension",
+  iconUrl: "/contest/mona-lisa-moustache.png",
+  nativeCurrency: {
+    decimals: 18,
+    name: "DYM",
+    symbol: "DYM",
+  },
+  rpcUrls: {
+    public: {
+      http: ["https://jsonrpc.dymension.nodestake.org"],
+    },
+    default: {
+      http: ["https://jsonrpc.dymension.nodestake.org"],
+    },
+  },
+  blockExplorers: {
+    etherscan: { name: "Dymension Mainnet Explorer", url: "https://dym.fyi/" },
+    default: { name: "Dymension Mainnet Explorer", url: "https://dym.fyi/" },
+  },
+};
