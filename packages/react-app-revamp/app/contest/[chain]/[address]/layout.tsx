bca19f601e8f54c322c360cdf1c3ec358c3ec515
blazh <medenicablazo@gmail.com>
2024-04-02 13:15:45 +0200

    chore: bring back original contest layout

--- packages/react-app-revamp/app/contest/[chain]/[address]/layout.tsx
+++ packages/react-app-revamp/app/contest/[chain]/[address]/layout.tsx
@@ -1,8 +1,32 @@
 "use client";
+import { CastVotesWrapper } from "@hooks/useCastVotes/store";
+import { ContestWrapper } from "@hooks/useContest/store";
+import { ContractFactoryWrapper } from "@hooks/useContractFactory";
+import { DeleteProposalWrapper } from "@hooks/useDeleteProposal/store";
+import { ProposalWrapper } from "@hooks/useProposal/store";
+import { RewardsWrapper } from "@hooks/useRewards/store";
+import { UserWrapper } from "@hooks/useUser/store";
+import LayoutViewContest from "@layouts/LayoutViewContest";
 import React from "react";
 
 const ContestLayout = ({ children }: { children: React.ReactNode }) => {
-  return <p>lol</p>;
+  return (
+    <ContestWrapper>
+      <ProposalWrapper>
+        <DeleteProposalWrapper>
+          <UserWrapper>
+            <CastVotesWrapper>
+              <ContractFactoryWrapper>
+                <RewardsWrapper>
+                  <LayoutViewContest>{children}</LayoutViewContest>
+                </RewardsWrapper>
+              </ContractFactoryWrapper>
+            </CastVotesWrapper>
+          </UserWrapper>
+        </DeleteProposalWrapper>
+      </ProposalWrapper>
+    </ContestWrapper>
+  );
 };
 
 export default ContestLayout;
