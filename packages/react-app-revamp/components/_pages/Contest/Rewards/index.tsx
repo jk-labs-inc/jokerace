040abad65ff68641b54e67a39a7f49f8a05c4302
blazh <medenicablazo@gmail.com>
2024-05-21 15:56:28 +0200

    enhancement: add initial work

--- packages/react-app-revamp/components/_pages/Contest/Rewards/index.tsx
+++ packages/react-app-revamp/components/_pages/Contest/Rewards/index.tsx
@@ -103,22 +103,31 @@ const ContestRewards = () => {
 
     return (
       <div className="flex flex-col gap-12">
-        <p className="text-[24px] font-bold text-neutral-11">create a rewards pool</p>
-        <div className="flex flex-col gap-4 text-[16px] text-neutral-11">
-          <p>a rewards pool incentivizes players, compensates winners, and helps showcase you to players.</p>
-          <p>
-            when you create a rewards pool, you’ll set what percent of rewards <br />
-            each winner receives—and then you’ll have the option to fund it
-            <br /> with whatever tokens you like.
+        <div className="flex flex-col gap-4">
+          <p className="text-[24px] font-bold text-true-white">let’s add rewards 💸💸💸</p>
+          <p className="text-[16px] text-neutral-11">what’s a good contest without rewards?</p>
+          <p className="text-[16px] text-neutral-11">
+            add rewards to get more players, up the stakes, and give a better <br />
+            time for all.
           </p>
-          <ButtonV3
-            colorClass={`bg-gradient-create rounded-[40px] mt-3`}
-            size={ButtonSize.LARGE}
-            onClick={() => setIsDeployRewardsOpen(true)}
-          >
-            create rewards pool
-          </ButtonV3>
+          <p className="text-[16px] text-neutral-11">for each option, we’ll create a rewards pool and then fund it.</p>
         </div>
+        <div className="flex flex-col gap-8">
+          <div className="flex flex-col gap-4 w-full md:w-[360px] h-48 pt-4 pb-6 pl-4 border-neutral-9 border rounded-[10px] hover:border-neutral-11 transition-colors duration-300">
+            <p className="text-neutral-11 text-[20px] font-bold">reward winners</p>
+            <p className="text-neutral-14 text-[16px]">
+              create and fund a pool to distribute <br /> tokens proportionately to winners
+            </p>
+            <ButtonV3
+              colorClass={`bg-gradient-vote text-[16px] rounded-[30px] font-bold text-true-black pb-2 pt-1`}
+              size={ButtonSize.EXTRA_LARGE}
+              onClick={() => setIsDeployRewardsOpen(true)}
+            >
+              create rewards pool
+            </ButtonV3>
+          </div>
+        </div>
+
         <DialogModalV3
           isOpen={isDeployRewardsOpen}
           setIsOpen={value => setIsDeployRewardsOpen(value)}
