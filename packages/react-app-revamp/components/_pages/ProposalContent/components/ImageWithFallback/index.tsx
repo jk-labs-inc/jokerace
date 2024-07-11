8f0c095190167beda1ceafaadd778b5438f6a44d
blazh <medenicablazo@gmail.com>
2024-07-11 14:42:25 +0200

    enhancement: add option to expand image in proposal

--- packages/react-app-revamp/components/_pages/ProposalContent/components/ImageWithFallback/index.tsx
+++ packages/react-app-revamp/components/_pages/ProposalContent/components/ImageWithFallback/index.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
+import { ArrowsPointingInIcon, ArrowsPointingOutIcon } from "@heroicons/react/24/outline";
 import { useEffect, useState } from "react";
 
 interface ImageWithFallbackProps {
@@ -8,6 +9,7 @@ interface ImageWithFallbackProps {
 }
 
 const ImageWithFallback: React.FC<ImageWithFallbackProps> = ({ src, fallbackSrc, alt }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
   const [imgSrc, setImgSrc] = useState(src);
 
   useEffect(() => {
@@ -17,7 +19,30 @@ const ImageWithFallback: React.FC<ImageWithFallbackProps> = ({ src, fallbackSrc,
     img.onerror = () => setImgSrc(fallbackSrc);
   }, [src, fallbackSrc]);
 
-  return <img src={imgSrc} alt={alt} className="rounded-[16px]" />;
+  const toggleExpand = (e: React.MouseEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setIsExpanded(!isExpanded);
+    setImgSrc(isExpanded ? src : fallbackSrc.replace("-medium", ""));
+  };
+
+  return (
+    <div className="relative inline-block">
+      <img src={imgSrc} alt={alt} className={`max-w-full h-auto ${isExpanded ? "w-full" : ""}`} />
+      <div className="absolute top-0 right-0 p-1">
+        <button
+          onClick={toggleExpand}
+          className="bg-true-black bg-opacity-50 text-neutral-11 p-1 rounded-full hover:bg-opacity-75 transition-all z-10"
+        >
+          {isExpanded ? (
+            <ArrowsPointingInIcon className="w-6 h-6 text-neutral-11" />
+          ) : (
+            <ArrowsPointingOutIcon className="w-6 h-6 text-neutral-11" />
+          )}
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default ImageWithFallback;
