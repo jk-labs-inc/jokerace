a643528a8f3be4ab4c4db977c935664ffd572d1e
blazh <medenicablazo@gmail.com>
2024-07-11 14:50:53 +0200

    enhancement: only apply expand func if image has variants

--- packages/react-app-revamp/components/_pages/ProposalContent/components/ImageWithFallback/index.tsx
+++ packages/react-app-revamp/components/_pages/ProposalContent/components/ImageWithFallback/index.tsx
@@ -11,36 +11,45 @@ interface ImageWithFallbackProps {
 const ImageWithFallback: React.FC<ImageWithFallbackProps> = ({ src, fallbackSrc, alt }) => {
   const [isExpanded, setIsExpanded] = useState(false);
   const [imgSrc, setImgSrc] = useState(src);
+  const [hasMediumVersion, setHasMediumVersion] = useState(false);
 
   useEffect(() => {
-    const img = new Image();
-    img.src = src;
-    img.onload = () => setImgSrc(src);
-    img.onerror = () => setImgSrc(fallbackSrc);
+    const mediumImg = new Image();
+    mediumImg.src = src;
+    mediumImg.onload = () => {
+      setImgSrc(src);
+      setHasMediumVersion(true);
+    };
+    mediumImg.onerror = () => {
+      setImgSrc(fallbackSrc);
+      setHasMediumVersion(false);
+    };
   }, [src, fallbackSrc]);
 
   const toggleExpand = (e: React.MouseEvent) => {
     e.preventDefault();
     e.stopPropagation();
     setIsExpanded(!isExpanded);
-    setImgSrc(isExpanded ? src : fallbackSrc.replace("-medium", ""));
+    setImgSrc(isExpanded ? src : fallbackSrc);
   };
 
   return (
     <div className="relative inline-block">
-      <img src={imgSrc} alt={alt} className={`max-w-full h-auto ${isExpanded ? "w-full" : ""}`} />
-      <div className="absolute top-0 right-0 p-1">
-        <button
-          onClick={toggleExpand}
-          className="bg-true-black bg-opacity-50 text-neutral-11 p-1 rounded-full hover:bg-opacity-75 transition-all z-10"
-        >
-          {isExpanded ? (
-            <ArrowsPointingInIcon className="w-6 h-6 text-neutral-11" />
-          ) : (
-            <ArrowsPointingOutIcon className="w-6 h-6 text-neutral-11" />
-          )}
-        </button>
-      </div>
+      <img src={imgSrc} alt={alt} />
+      {hasMediumVersion && (
+        <div className="absolute top-0 right-0 p-1">
+          <button
+            onClick={toggleExpand}
+            className="bg-true-black bg-opacity-50 text-neutral-11 p-1 rounded-full hover:bg-opacity-75 transition-all z-10"
+          >
+            {isExpanded ? (
+              <ArrowsPointingInIcon className="w-6 h-6 text-neutral-11" />
+            ) : (
+              <ArrowsPointingOutIcon className="w-6 h-6 text-neutral-11" />
+            )}
+          </button>
+        </div>
+      )}
     </div>
   );
 };
