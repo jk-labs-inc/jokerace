b497708bfd2a3ad2aa6bc24d08b8dd62384cbd65
blazh <medenicablazo@gmail.com>
2024-07-26 14:24:42 +0200

    enhancement: apply text for mobile in explainer section

--- packages/react-app-revamp/components/_pages/Landing/components/Explainer/index.tsx
+++ packages/react-app-revamp/components/_pages/Landing/components/Explainer/index.tsx
@@ -43,26 +43,38 @@ const LandingPageExplainer = () => {
         <p className="text-neutral-11 text-[20px] md:text-[24px] font-bold">why jokerace?</p>
         <ul className="flex flex-col pl-8">
           <li className="text-[16px] md:text-[20px] text-neutral-11 list-disc">
-            contest creators make money: we split all earnings 50/50
+            {isMobile
+              ? "contest creators make money: we split earnings"
+              : "contest creators make money: we split all earnings 50/50"}
           </li>
           <li className="text-[16px] md:text-[20px] text-neutral-11 list-disc">
-            players can earn rewards, points, credentials, and more
+            {isMobile
+              ? "players can earn rewards, points, credentials, etc"
+              : "players can earn rewards, points, credentials, and more"}
           </li>
           <li className="text-[16px] md:text-[20px] text-neutral-11 list-disc">
-            rewards are fully programmable and verifiable onchain
+            {isMobile
+              ? "rewards are programmable & verifiable onchain"
+              : "rewards are fully programmable and verifiable onchain"}
           </li>
           <li className="text-[16px] md:text-[20px] text-neutral-11 list-disc">
             anyone can build and access extensions on us
           </li>
           <li className="text-[16px] md:text-[20px] text-neutral-11 list-disc">
-            players can perform any onchain action with their fees
+            {isMobile
+              ? "players’ fees can perform any onchain action"
+              : "players can perform any onchain action with their fees"}
           </li>
-          <li className="text-[16px] md:text-[20px] text-neutral-11 list-disc">
-            players build out social graphs of taste and attestations
-          </li>
-          <li className="text-[16px] md:text-[20px] text-neutral-11 list-disc">
-            contests are optimized to be <i>social</i>
-          </li>
+          {!isMobile && (
+            <>
+              <li className="text-[16px] md:text-[20px] text-neutral-11 list-disc">
+                players build out social graphs of taste and attestations
+              </li>
+              <li className="text-[16px] md:text-[20px] text-neutral-11 list-disc">
+                contests are optimized to be <i>social</i>
+              </li>
+            </>
+          )}
         </ul>
         <Subscribe />
       </div>
@@ -72,27 +84,50 @@ const LandingPageExplainer = () => {
           <p className="text-neutral-11 text-[16px] md:text-[20px] font-bold">to create a contest:</p>
           <ul className="flex flex-col pl-8">
             <li className="text-[16px] md:text-[20px] text-neutral-11 list-disc">
-              let anyone submit and vote on entries—or use allowlists
+              {isMobile
+                ? "let anyone submit and vote—or use allowlists"
+                : "let anyone submit and vote on entries—or use allowlists"}
             </li>
             <li className="text-[16px] md:text-[20px] text-neutral-11 list-disc">
               set the duration for submission and voting
             </li>
             <li className="text-[16px] md:text-[20px] text-neutral-11 list-disc">(optional) add rewards for winners</li>
             <li className="text-[16px] md:text-[20px] mt-4 md:mt-6 text-neutral-11 list-disc">
-              <b>
-                <i>that’s it.</i>
-              </b>{" "}
-              you just pay the cost to deploy (often just cents).
+              {isMobile ? (
+                <>
+                  <b>
+                    <i>that’s it.</i>
+                  </b>{" "}
+                  you just pay the cost to deploy
+                </>
+              ) : (
+                <>
+                  <b>
+                    <i>that’s it.</i>
+                  </b>{" "}
+                  you just pay the cost to deploy (often just cents).
+                </>
+              )}
             </li>
           </ul>
         </div>
         <p className="text-neutral-11 text-[16px] md:text-[20px]">
-          <i>pro tip:</i> run a <i>quality</i> contest for a jury to pick finalists; then run <br />
-          an <i>engagement</i> contest for anyone to vote on the winner.
+          {isMobile ? (
+            <>
+              <i>tip:</i> first run a contest for a jury to pick top finalists; then run one for anyone to vote on the
+              winner.
+            </>
+          ) : (
+            <>
+              {" "}
+              <i>pro tip:</i> run a <i>quality</i> contest for a jury to pick finalists; then run <br />
+              an <i>engagement</i> contest for anyone to vote on the winner.
+            </>
+          )}
         </p>
         <Link
           href={ROUTE_CREATE_CONTEST}
-          className="w-[218px] h-10 bg-gradient-purple-white rounded-[40px] normal-case text-[16px] md:text-[20px] font-bold text-true-black flex items-center justify-center relative"
+          className="w-[218px] h-10 bg-gradient-purple-white rounded-[40px] normal-case text-[20px] font-bold text-true-black flex items-center justify-center relative"
         >
           <span className="mr-4 normal-case">Create a Contest</span>
           <ChevronRightIcon className="w-6 h-6 text-true-black font-bold absolute right-2" />
@@ -109,12 +144,14 @@ const LandingPageExplainer = () => {
             vote on entries during the voting period
           </li>
           <li className="text-[16px] md:text-[20px] text-neutral-11 list-disc">
-            earn rewards, attestations, points, credentials, and more.
+            {isMobile
+              ? "earn rewards, attestations, points, credentials"
+              : "earn rewards, attestations, points, credentials, and more."}
           </li>
         </ul>
         <Link
           href={ROUTE_VIEW_LIVE_CONTESTS}
-          className="w-[218px] h-10 bg-gradient-purple-white rounded-[40px] normal-case text-[16px] md:text-[20px] font-bold text-true-black flex items-center justify-center relative"
+          className="w-[218px] h-10 bg-gradient-purple-white rounded-[40px] normal-case text-[20px] font-bold text-true-black flex items-center justify-center relative"
         >
           <span className="mr-4 normal-case">View Contests</span>
           <ChevronRightIcon className="w-6 h-6 text-true-black font-bold absolute right-2" />
@@ -125,22 +162,37 @@ const LandingPageExplainer = () => {
         <p className="text-neutral-11 text-[20px] md:text-[24px] font-bold">to build on our protocol:</p>
         <ul className="flex flex-col pl-8">
           <li className="text-[16px] md:text-[20px] text-neutral-11 list-disc">
-            we’re open source and onchain—so anyone can build on us
+            {isMobile
+              ? "it’s permissionless: we’re open source onchain"
+              : "we’re open source and onchain—so anyone can build on us"}
           </li>
           <li className="text-[16px] md:text-[20px] text-neutral-11 list-disc">
-            build an extension to extend contest features and drive <br /> users to your service (ie analytics, points,
-            predictions, etc.)
+            {isMobile ? (
+              "build an extension to extend contest features and drive users to your service"
+            ) : (
+              <>
+                build an extension to extend contest features and drive <br /> users to your service (ie analytics,
+                points, predictions, etc.)
+              </>
+            )}
           </li>
           <li className="text-[16px] md:text-[20px] text-neutral-11 list-disc">
-            build a circuit: a smart contract that lets players perform <br /> any action with their fees (buy tokens,
-            products, etc)
+            {isMobile ? (
+              "build a circuit: a smart contract that lets players perform any action with their fees"
+            ) : (
+              <>
+                {" "}
+                build a circuit: a smart contract that lets players perform <br /> any action with their fees (buy
+                tokens, products, etc)
+              </>
+            )}
           </li>
         </ul>
         <a
           href="https://docs.jokerace.io/"
           target="_blank"
           rel="noopener noreferrer"
-          className="w-[218px] h-10 bg-gradient-purple-white rounded-[40px] normal-case text-[16px] md:text-[20px] font-bold text-true-black flex items-center justify-center relative"
+          className="w-[218px] h-10 bg-gradient-purple-white rounded-[40px] normal-case text-[20px] font-bold text-true-black flex items-center justify-center relative"
         >
           <span className="mr-4 normal-case">Read Our Docs</span>
           <ChevronRightIcon className="w-6 h-6 text-true-black font-bold absolute right-2" />
