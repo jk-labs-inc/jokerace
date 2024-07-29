38e40ddf1b7f05e5cc00b2fc3c1cdb1a1932cd88
Sean McCaffery <sean.mccaffery9@gmail.com>
2024-07-29 11:52:28 -0400

    chore: update db schema

--- packages/react-app-revamp/supabase_schemas/chain_params.sql
+++ packages/react-app-revamp/supabase_schemas/chain_params.sql
@@ -4,5 +4,6 @@ create table
     network_name character varying not null,
     min_cost_to_propose numeric not null,
     min_cost_to_vote numeric not null,
+    jk_labs_split_destination character varying null,
     constraint chain_params_pkey primary key (id)
   ) tablespace pg_default;
