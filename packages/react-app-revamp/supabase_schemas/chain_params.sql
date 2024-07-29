551bb093e4ca80ffd7fafbb0fb72d4b1203cbb3e
Sean McCaffery <sean.mccaffery9@gmail.com>
2024-07-29 12:33:22 -0400

    chore: update schema to be not null with default

--- packages/react-app-revamp/supabase_schemas/chain_params.sql
+++ packages/react-app-revamp/supabase_schemas/chain_params.sql
@@ -4,6 +4,6 @@ create table
     network_name character varying not null,
     min_cost_to_propose numeric not null,
     min_cost_to_vote numeric not null,
-    jk_labs_split_destination character varying null,
+    jk_labs_split_destination character varying not null default '0xDc652C746A8F85e18Ce632d97c6118e8a52fa738'::character varying,
     constraint chain_params_pkey primary key (id)
   ) tablespace pg_default;
