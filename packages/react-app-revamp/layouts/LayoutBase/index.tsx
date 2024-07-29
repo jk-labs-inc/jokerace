ce087dfdca8c6c83d1be8e525ccda9b20ea56522
blazh <medenicablazo@gmail.com>
2024-07-29 11:52:52 +0200

    enhancement: increase padding bottom on base layout

--- packages/react-app-revamp/layouts/LayoutBase/index.tsx
+++ packages/react-app-revamp/layouts/LayoutBase/index.tsx
@@ -17,7 +17,7 @@ const LayoutBase = (props: LayoutBaseProps) => {
   return (
     <>
       <Header />
-      <main className={`flex flex-col grow ${isCreateFlow ? "pb-72" : "pb-28"} md:pb-0`}>{children}</main>
+      <main className={`flex flex-col grow ${isCreateFlow ? "pb-72" : "pb-40"} md:pb-0`}>{children}</main>
       <footer className="mt-auto top-0 xs:pt-32">
         {/* //todo: adjust for mobile */}
         <div className="text-neutral-10 container justify-center items-start text-[12px] md:text-[16px] hidden md:flex pl-8 pr-8 md:pl-16 md:pr-16 space-y-1 xs:space-y-0 xs:space-i-4  mx-auto">
