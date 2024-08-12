6a15156191af335459885f7097e4d338a1065b99
Sean McCaffery <sean.mccaffery9@gmail.com>
2024-08-12 14:09:39 -0400

    chain: add forma and formaTestnet

---
+++ packages/react-app-revamp/config/wagmi/custom-chains/forma.ts
@@ -0,0 +1,25 @@
+import { Chain } from "@rainbow-me/rainbowkit";
+
+export const forma: Chain = {
+  id: 984122,
+  name: "Forma",
+  iconUrl: "/contest/mona-lisa-moustache.png",
+  nativeCurrency: {
+    decimals: 18,
+    name: "TIA",
+    symbol: "TIA",
+  },
+  rpcUrls: {
+    public: {
+      http: ["https://rpc.forma.art"],
+    },
+    default: {
+      http: ["https://rpc.forma.art"],
+    },
+  },
+  blockExplorers: {
+    etherscan: { name: "Forma Block Explorer", url: "https://explorer.forma.art" },
+    default: { name: "Forma Block Explorer", url: "https://explorer.forma.art" },
+  },
+  testnet: false,
+};
---
+++ packages/react-app-revamp/config/wagmi/custom-chains/formaTestnet.ts
@@ -0,0 +1,25 @@
+import { Chain } from "@rainbow-me/rainbowkit";
+
+export const formaTestnet: Chain = {
+  id: 984123,
+  name: "Forma Testnet",
+  iconUrl: "/contest/mona-lisa-moustache.png",
+  nativeCurrency: {
+    decimals: 18,
+    name: "TIA",
+    symbol: "TIA",
+  },
+  rpcUrls: {
+    public: {
+      http: ["https://rpc.sketchpad-1.forma.art"],
+    },
+    default: {
+      http: ["https://rpc.sketchpad-1.forma.art"],
+    },
+  },
+  blockExplorers: {
+    etherscan: { name: "Forma Block Explorer", url: "https://explorer.sketchpad-1.forma.art" },
+    default: { name: "Forma Block Explorer", url: "https://explorer.sketchpad-1.forma.art" },
+  },
+  testnet: true,
+};
