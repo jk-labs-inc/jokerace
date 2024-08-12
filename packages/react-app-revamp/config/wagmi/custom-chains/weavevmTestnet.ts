cd3129a1ef9b4bf7ab40ba8a042e6809b4b5ca8f
Sean McCaffery <sean.mccaffery9@gmail.com>
2024-08-12 09:14:58 -0400

    chain: add weavevm testnet

---
+++ packages/react-app-revamp/config/wagmi/custom-chains/weavevmTestnet.ts
@@ -0,0 +1,24 @@
+import { Chain } from "@rainbow-me/rainbowkit";
+
+export const weavevmTestnet: Chain = {
+  id: 9496,
+  name: "WeaveVM",
+  iconUrl: "/wvm.svg",
+  nativeCurrency: {
+    decimals: 18,
+    name: "tWVM",
+    symbol: "tWVM",
+  },
+  rpcUrls: {
+    public: {
+      http: ["https://testnet-rpc.wvm.dev"],
+    },
+    default: {
+      http: ["https://testnet-rpc.wvm.dev"],
+    },
+  },
+  blockExplorers: {
+    etherscan: { name: "WeaveVM Block Explorer", url: "https://explorer.wvm.dev" },
+    default: { name: "WeaveVM Block Explorer", url: "https://explorer.wvm.dev" },
+  },
+};
