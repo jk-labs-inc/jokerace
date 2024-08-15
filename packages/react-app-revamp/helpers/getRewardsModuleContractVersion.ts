84466ae39d5877105b61f580a6d4469553e8115d
Sean McCaffery <sean.mccaffery9@gmail.com>
2024-08-15 09:09:04 -0400

    chore: check canceled

--- packages/react-app-revamp/helpers/getRewardsModuleContractVersion.ts
+++ packages/react-app-revamp/helpers/getRewardsModuleContractVersion.ts
@@ -52,6 +52,7 @@ import UpdateForgeLibsRewards from "@contracts/bytecodeAndAbi/modules/RewardsMod
 import SetSplitDestinationRewards from "@contracts/bytecodeAndAbi/modules/RewardsModule.4.29.setSplitDestination.sol/RewardsModule.json";
 import MakeJkLabsSplitConfigurableRewards from "@contracts/bytecodeAndAbi/modules/RewardsModule.4.30.makeJkLabsSplitConfigurable.sol/RewardsModule.json";
 import AddMetadataFieldsRewards from "@contracts/bytecodeAndAbi/modules/RewardsModule.4.31.addMetadataFields.sol/RewardsModule.json";
+import CheckCanceledRewards from "@contracts/bytecodeAndAbi/modules/RewardsModule.4.32.checkCanceled.sol/RewardsModule.json";
 import DeployedRewardsContract from "@contracts/bytecodeAndAbi/modules/RewardsModule.sol/RewardsModule.json";
 import { ethers } from "ethers";
 import { getEthersProvider } from "./ethers";
@@ -63,7 +64,9 @@ export async function getRewardsModuleContractVersion(address: string, chainId: 
 
   try {
     const version: string = await executeWithTimeout(MAX_TIME_TO_WAIT_FOR_RPC, contract.version());
-    if (version === "4.31") {
+    if (version === "4.32") {
+      return CheckCanceledRewards.abi;
+    } else if (version === "4.31") {
       return AddMetadataFieldsRewards.abi;
     } else if (version === "4.30") {
       return MakeJkLabsSplitConfigurableRewards.abi;
