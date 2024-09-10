ebf8b4b8dc779153cd8745db88f26078f0c67e0b
blazh <medenicablazo@gmail.com>
2024-09-10 13:06:16 +0200

    feat: add switchChain fn to cancel rewards

--- packages/react-app-revamp/components/_pages/Contest/Rewards/components/CancelRewards/index.tsx
+++ packages/react-app-revamp/components/_pages/Contest/Rewards/components/CancelRewards/index.tsx
@@ -3,6 +3,9 @@ import { FC, useState } from "react";
 import CancelRewardsModal from "./components/Modal";
 import { TrashIcon } from "@heroicons/react/24/outline";
 import { Abi } from "viem";
+import { switchChain } from "@wagmi/core";
+import { useAccount } from "wagmi";
+import { config } from "@config/wagmi";
 
 interface CancelRewardsProps {
   rewardsAddress: `0x${string}`;
@@ -14,10 +17,15 @@ interface CancelRewardsProps {
 const CancelRewards: FC<CancelRewardsProps> = ({ rewardsAddress, abi, chainId, version }) => {
   const { cancelRewards, isLoading } = useCancelRewards({ rewardsAddress, abi, chainId, version });
   const [isCancelRewardsModalOpen, setIsCancelRewardsModalOpen] = useState(false);
-
+  const { chainId: accountChainId } = useAccount();
+  const isUserOnCorrectChain = accountChainId === chainId;
   const handleOpenModal = () => setIsCancelRewardsModalOpen(true);
 
   const handleCancelRewards = async () => {
+    if (!isUserOnCorrectChain) {
+      await switchChain(config, { chainId });
+    }
+
     cancelRewards();
     setIsCancelRewardsModalOpen(false);
   };
