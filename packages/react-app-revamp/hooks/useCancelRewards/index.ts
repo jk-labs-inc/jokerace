9600d02ece30313f285ae1c33a385c40cf4a140b
zho <medenicablazo@gmail.com>
2024-09-10 18:35:52 +0200

    feat: add cancel rewards functionality (#2321)
    * feat: add cancel rewards functionality
    
    * feat: check for rewards status in rewards info as well
    
    * feat: add switchChain fn to cancel rewards
    
    * feat: add toasts and one more messsage in cancel rewards modal

---
+++ packages/react-app-revamp/hooks/useCancelRewards/index.ts
@@ -0,0 +1,77 @@
+import { toastLoading, toastSuccess } from "@components/UI/Toast";
+import { useError } from "@hooks/useError";
+import { compareVersions } from "compare-versions";
+import { useEffect } from "react";
+import { Abi } from "viem";
+import { useReadContract, useWaitForTransactionReceipt, useWriteContract } from "wagmi";
+
+interface UseCancelRewardsParams {
+  rewardsAddress: `0x${string}`;
+  abi: Abi;
+  chainId: number;
+  version: string;
+}
+
+export const REWARDS_CANCELED_VERSION = "4.33";
+
+export function useCancelRewards({ rewardsAddress, abi, chainId, version }: UseCancelRewardsParams) {
+  const hasCanceledFunction = compareVersions(version, REWARDS_CANCELED_VERSION) >= 0;
+  const { handleError } = useError();
+
+  const {
+    data: isCanceled,
+    isLoading: isReadLoading,
+    isSuccess: isReadSuccess,
+    isError: isReadError,
+    refetch,
+  } = useReadContract({
+    address: rewardsAddress,
+    abi,
+    functionName: "canceled",
+    chainId,
+    query: {
+      enabled: hasCanceledFunction,
+    },
+  });
+
+  const { writeContract, data: hash, isPending: isWritePending, isError: isWriteError } = useWriteContract();
+  const { isLoading: isConfirming, isSuccess: isConfirmed } = useWaitForTransactionReceipt({
+    hash,
+  });
+
+  const cancelRewards = async () => {
+    if (!hasCanceledFunction) return;
+
+    toastLoading("Cancelling rewards..");
+
+    try {
+      writeContract({
+        address: rewardsAddress,
+        abi,
+        functionName: "cancel",
+        chainId,
+      });
+    } catch (error) {
+      handleError(error, "error canceling rewards");
+    }
+  };
+
+  useEffect(() => {
+    if (isConfirmed) {
+      toastSuccess("Rewards cancelled!");
+      refetch();
+    }
+  }, [isConfirmed, refetch]);
+
+  return {
+    isCanceled: hasCanceledFunction ? !!isCanceled : false,
+    isLoading: hasCanceledFunction ? isReadLoading : false,
+    isSuccess: hasCanceledFunction ? isReadSuccess : true,
+    isError: hasCanceledFunction ? isReadError : false,
+    cancelRewards,
+    isWritePending,
+    isWriteError,
+    isConfirming,
+    isConfirmed,
+  };
+}
