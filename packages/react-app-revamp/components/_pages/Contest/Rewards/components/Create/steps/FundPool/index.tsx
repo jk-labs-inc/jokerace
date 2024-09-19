94d236af3c72fd466b9d7ebeb7b33ae359e6a8bf
blazh <medenicablazo@gmail.com>
2024-09-19 14:48:38 +0200

    enhancement: reduce space between elements in funds section to 48px

--- packages/react-app-revamp/components/_pages/Contest/Rewards/components/Create/steps/FundPool/index.tsx
+++ packages/react-app-revamp/components/_pages/Contest/Rewards/components/Create/steps/FundPool/index.tsx
@@ -17,7 +17,7 @@ const CreateRewardsFundPool = () => {
   const allTokensUnique = tokenWidgets.length === new Set(tokenWidgets.map(token => token.address)).size;
 
   return (
-    <div className="flex flex-col gap-16 animate-swingInLeft">
+    <div className="flex flex-col gap-12 animate-swingInLeft">
       <div className="flex flex-col gap-4">
         <p className="text-[24px] font-bold text-true-white">time to have fun(ds)</p>
         <p className="text-[16px] text-neutral-11">now let’s fund your rewards pool.</p>
