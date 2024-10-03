9b1ea73779edd290b998d4b06f2a45639f8dbbce
zho <medenicablazo@gmail.com>
2024-10-03 15:46:28 +0200

    enhancement: apply wallet/profile switch for mobile (#2552)
    * enhancement: apply wallet/profile switch for mobile
    
    * enhancement: fix build error

--- packages/react-app-revamp/components/_pages/Contest/components/ContestName/index.tsx
+++ packages/react-app-revamp/components/_pages/Contest/components/ContestName/index.tsx
@@ -1,8 +1,10 @@
+import BurgerMenu from "@components/UI/BurgerMenu";
 import GradientText from "@components/UI/GradientText";
+import { FOOTER_LINKS } from "@config/links";
 import { ContestStateEnum, useContestStateStore } from "@hooks/useContestState/store";
 import { FC } from "react";
-import CancelContest from "../CancelContest";
 import { useMediaQuery } from "react-responsive";
+import CancelContest from "../CancelContest";
 
 interface ContestNameProps {
   contestName: string;
@@ -12,12 +14,34 @@ const ContestName: FC<ContestNameProps> = ({ contestName }) => {
   const { contestState } = useContestStateStore(state => state);
   const isContestCanceled = contestState === ContestStateEnum.Canceled;
   const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
+  const allowedLinks = ["Github", "Twitter", "Telegram", "Report a bug", "Terms", "Media Kit"];
+  const filteredLinks = FOOTER_LINKS.filter(link => allowedLinks.includes(link.label));
 
   return (
-    <div className={`flex items-center justify-between md:justify-normal`}>
-      {!isMobile && <CancelContest />}
+    <div className={`flex items-center justify-between ${isMobile ? "w-full" : ""}`}>
       <GradientText text={contestName} isStrikethrough={isContestCanceled} />
-      {isMobile && <CancelContest />}
+      {isMobile ? (
+        <div className="flex flex-col items-center gap-2">
+          <BurgerMenu>
+            <div className="flex justify-end flex-col gap-2">
+              {filteredLinks.map((link, key) => (
+                <a
+                  className="font-sabo text-neutral-11 text-[24px]"
+                  key={`footer-link-${key}`}
+                  href={link.href}
+                  rel="nofollow noreferrer"
+                  target="_blank"
+                >
+                  {link.label}
+                </a>
+              ))}
+            </div>
+          </BurgerMenu>
+          <CancelContest />
+        </div>
+      ) : (
+        <CancelContest />
+      )}
     </div>
   );
 };
