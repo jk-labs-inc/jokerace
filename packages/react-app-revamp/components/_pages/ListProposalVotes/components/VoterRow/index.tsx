19430096f2b7dc5d4f91983e0f311183231975ff
blazh <medenicablazo@gmail.com>
2024-10-04 13:38:51 +0200

    bugfix: apply padding only when scrollbar is appended

--- packages/react-app-revamp/components/_pages/ListProposalVotes/components/VoterRow/index.tsx
+++ packages/react-app-revamp/components/_pages/ListProposalVotes/components/VoterRow/index.tsx
@@ -1,5 +1,6 @@
 import UserProfileDisplay from "@components/UI/UserProfileDisplay";
 import { formatNumberAbbreviated } from "@helpers/formatNumber";
+import { VOTES_PER_PAGE } from "@hooks/useProposalVotes";
 
 interface VoterRowData {
   votesPerAddress: Record<string, number>;
@@ -9,10 +10,12 @@ interface VoterRowData {
 const VoterRow = ({ index, style, data }: { index: number; style: React.CSSProperties; data: VoterRowData }) => {
   const { votesPerAddress, addresses } = data;
   const address = addresses[index];
+  const needsPadding = addresses.length > VOTES_PER_PAGE;
+
   return (
     <div
       style={style}
-      className="flex justify-between items-end text-[16px] font-bold pb-3 pr-2 border-b border-neutral-10"
+      className={`flex justify-between items-end text-[16px] font-bold pb-3 border-b border-neutral-10 ${needsPadding ? "pr-4" : ""}`}
     >
       <UserProfileDisplay ethereumAddress={address} shortenOnFallback={true} />
       <p>{formatNumberAbbreviated(votesPerAddress[address])} votes</p>
