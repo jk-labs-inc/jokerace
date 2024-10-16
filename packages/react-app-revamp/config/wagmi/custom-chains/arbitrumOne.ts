63700ce559b25a07ade5230923129bfce68cd1c9
Sean McCaffery <sean.mccaffery9@gmail.com>
2024-10-16 13:08:00 -0400

    chore: move to public arb rpc (#2716)

--- packages/react-app-revamp/config/wagmi/custom-chains/arbitrumOne.ts
+++ packages/react-app-revamp/config/wagmi/custom-chains/arbitrumOne.ts
@@ -11,10 +11,10 @@ export const arbitrumOne: Chain = {
   },
   rpcUrls: {
     public: {
-      http: [`https://${process.env.NEXT_PUBLIC_QUICKNODE_SLUG}.arbitrum-mainnet.quiknode.pro/${process.env.NEXT_PUBLIC_QUICKNODE_KEY}`],
+      http: ["https://arb1.arbitrum.io/rpc"],
     },
     default: {
-      http: [`https://${process.env.NEXT_PUBLIC_QUICKNODE_SLUG}.arbitrum-mainnet.quiknode.pro/${process.env.NEXT_PUBLIC_QUICKNODE_KEY}`],
+      http: ["https://arb1.arbitrum.io/rpc"],
     },
   },
   blockExplorers: {
