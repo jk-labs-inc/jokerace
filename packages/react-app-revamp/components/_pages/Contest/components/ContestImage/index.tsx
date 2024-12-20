ec1185bec8775e56efe6d492257c5fd15173fad9
blazh <medenicablazo@gmail.com>
2024-12-20 13:07:05 +0100

    enhancement: add blur container to contest image

--- packages/react-app-revamp/components/_pages/Contest/components/ContestImage/index.tsx
+++ packages/react-app-revamp/components/_pages/Contest/components/ContestImage/index.tsx
@@ -6,8 +6,19 @@ interface ContestImageProps {
 
 const ContestImage: FC<ContestImageProps> = ({ imageUrl }) => {
   return (
-    <div className="w-full min-h-[296px] bg-true-black rounded-[16px] border-true-black shadow-file-upload p-4">
-      <img src={imageUrl} alt="contest" className="w-full max-h-[264px] rounded-2xl object-fill" />
+    <div className="w-full min-h-[294px] rounded-2xl shadow-file-upload relative overflow-hidden">
+      {/* background blurred layer */}
+      <div
+        className="absolute inset-[-20px] bg-cover bg-center opacity-80 blur-[30px]"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+
+      {/* centered main image */}
+      <div className="relative h-full flex items-center justify-center">
+        <img src={imageUrl} alt="contest" className="max-w-full max-h-[294px] object-contain" />
+      </div>
     </div>
   );
 };
