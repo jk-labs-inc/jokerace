dcb22c893c6de7aa0e1450dd6bf6b6bc254a12ab
blazh <medenicablazo@gmail.com>
2024-12-23 13:31:04 +0100

    chore: update toast language for exectuion reverted errors

--- packages/react-app-revamp/hooks/useError/index.ts
+++ packages/react-app-revamp/hooks/useError/index.ts
@@ -1,8 +1,15 @@
 import { toastDismiss, toastError } from "@components/UI/Toast";
+import { chains } from "@config/wagmi";
+import { extractPathSegments } from "@helpers/extractPath";
+import { usePathname } from "next/navigation";
 import { useState } from "react";
 import { didUserReject, handleError as handleUtilityError } from "utils/error";
+import { useAccount } from "wagmi";
 
 export function useError() {
+  const pathname = usePathname();
+  const { chainName: chainNameFromPath } = extractPathSegments(pathname);
+  const { chain: chainFromAccount } = useAccount();
   const [error, setError] = useState<string>("");
 
   const handleError = (e: any, defaultMessage: string) => {
@@ -11,7 +18,9 @@ export function useError() {
       return;
     }
 
-    const handledError = handleUtilityError(e);
+    const chainName = resolveChainName(chainNameFromPath);
+
+    const handledError = handleUtilityError(e, chainName);
     setError(handledError.message);
 
     if (handledError.codeFound) {
@@ -19,6 +28,13 @@ export function useError() {
     } else {
       toastError(defaultMessage, handledError.message);
     }
+  };
+
+  const resolveChainName = (chainName: string) => {
+    if (chainName !== chains.filter(chain => chain.name.toLowerCase() === chainName.toLowerCase())[0]?.name) {
+      return chainFromAccount?.name ?? "";
+    }
+    return chainName;
   };
 
   return { error, handleError };
