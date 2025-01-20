ff2c5dfd944c5d508b757960c5e2870c01b7954b
blazh <medenicablazo@gmail.com>
2025-01-20 11:23:09 +0100

    enhhancement: fix dropdown chevron positioning in tag input in create flow

--- packages/react-app-revamp/components/_pages/Create/components/TextInput/index.tsx
+++ packages/react-app-revamp/components/_pages/Create/components/TextInput/index.tsx
@@ -1,4 +1,5 @@
 import { FC, useRef } from "react";
+import { ChevronDownIcon } from "@heroicons/react/24/outline";
 
 interface CreateTextInputProps {
   value?: string;
@@ -12,6 +13,8 @@ interface CreateTextInputProps {
   className?: string;
   onChange?: (value: string) => void;
   onClick?: () => void;
+  showChevron?: boolean;
+  isChevronUp?: boolean;
 }
 
 const CreateTextInput: FC<CreateTextInputProps> = ({
@@ -25,12 +28,13 @@ const CreateTextInput: FC<CreateTextInputProps> = ({
   className,
   onChange,
   onClick,
+  showChevron = false,
+  isChevronUp = false,
 }) => {
   const inputRef = useRef(null);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     let newValue = e.target.value;
-
     onChange?.(newValue);
   };
 
@@ -40,14 +44,16 @@ const CreateTextInput: FC<CreateTextInputProps> = ({
         contest title <span className="font-normal">(required)</span>
       </p>
       <div
-        className={`w-full md:w-[656px] bg-true-black rounded-[16px] border-true-black md:shadow-file-upload md:p-2`}
+        className={`w-full md:w-[656px] bg-true-black rounded-[16px] border-true-black md:shadow-file-upload md:p-2 relative`}
       >
         <input
           ref={inputRef}
           value={value}
           type="text"
           onClick={onClick}
-          className="text-[16px] bg-secondary-1 outline-none rounded-[16px] placeholder-neutral-10 w-full h-12 indent-4 focus:outline-none"
+          className={`text-[16px] bg-secondary-1 outline-none rounded-[16px] placeholder-neutral-10 w-full h-12 indent-4 focus:outline-none ${
+            showChevron ? "pr-10" : ""
+          }`}
           placeholder={placeholder}
           readOnly={readOnly}
           min={min}
@@ -56,6 +62,14 @@ const CreateTextInput: FC<CreateTextInputProps> = ({
           minLength={minLength}
           onChange={handleChange}
         />
+        {showChevron && (
+          <ChevronDownIcon
+            className={`w-5 cursor-pointer absolute right-4 top-1/2 -translate-y-1/2 transition-transform duration-200 ${
+              isChevronUp ? "rotate-180" : "rotate-0"
+            }`}
+            onClick={onClick}
+          />
+        )}
       </div>
     </div>
   );
