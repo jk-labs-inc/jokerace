2b9e7ea55c918446ecdd353cf8d83128940649cd
Sean McCaffery <smccaffery9@proton.me>
2025-02-15 00:24:03 -0500

    chore: rm import

--- packages/react-app-revamp/hooks/useProfileData/index.ts
+++ packages/react-app-revamp/hooks/useProfileData/index.ts
@@ -1,5 +1,4 @@
 import { Clusters } from "@clustersxyz/sdk";
-import { lensClient } from "@config/lens";
 import { config } from "@config/wagmi";
 import { mainnet } from "@config/wagmi/custom-chains/mainnet";
 import shortenEthereumAddress from "@helpers/shortenEthereumAddress";
