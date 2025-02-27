6bf05e3a1c58d85683fb9ba2c735e6307cee0d6b
blazh <medenicablazo@gmail.com>
2025-02-27 11:41:34 +0100

    enhancement: rm small redundancy

--- packages/react-app-revamp/hooks/useSetContestTemplate/index.ts
+++ packages/react-app-revamp/hooks/useSetContestTemplate/index.ts
@@ -3,14 +3,7 @@ import {
   useTimingOptionForVotingPeriod,
 } from "@components/_pages/Create/pages/ContestTiming/utils";
 import { TemplateConfig } from "@components/_pages/Create/templates/types";
-import { StepTitle } from "@components/_pages/Create/types";
 import { useDeployContestStore } from "@hooks/useDeployContest/store";
-
-const checkIfVotingNeeded = (steps: StepTitle[]): { isVotingNeeded: boolean } => {
-  return {
-    isVotingNeeded: steps.includes(StepTitle.Voting),
-  };
-};
 
 const useSetContestTemplate = () => {
   const {
@@ -27,15 +20,12 @@ const useSetContestTemplate = () => {
   const { setTimingOption: setVotingTimingOption } = useTimingOptionForVotingPeriod(state => state);
 
   const setContestTemplateConfig = (config: TemplateConfig) => {
-    const { isVotingNeeded } = checkIfVotingNeeded(config.stepsToFulfill);
-
     setContestType(config.data.contestType);
     setTitle(config.data.rules.title);
     setPrompt(config.data.rules.prompt);
     setSubmissionOpen(config.data.submissionOpen);
     setVotingOpen(config.data.votingOpen);
     setVotingClose(config.data.votingClose);
-    setVotingTab(isVotingNeeded ? 1 : 0);
     setSubmissionTimingOption(config.data.votingOpenPeriod);
     setVotingTimingOption(config.data.votingClosePeriod);
     setEntryPreviewConfig(config.data.entryPreviewConfig);
