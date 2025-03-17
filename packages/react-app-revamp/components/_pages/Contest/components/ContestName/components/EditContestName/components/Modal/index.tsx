b33f99566da3a7823c8236063100d28ed21d7a41
zho <medenicablazo@gmail.com>
2025-03-17 10:48:44 +0100

    chore: be resilient about r2 failures (#3467)
    * chore: test some r2 stuff
    
    * chore: more work on image upload component
    
    * chore: enhance image upload component
    
    * chore: clean image upload component of unused props
    
    * chore: clean components and reduce complexity

--- packages/react-app-revamp/components/_pages/Contest/components/ContestName/components/EditContestName/components/Modal/index.tsx
+++ packages/react-app-revamp/components/_pages/Contest/components/ContestName/components/EditContestName/components/Modal/index.tsx
@@ -1,8 +1,6 @@
 import { CONTEST_TITLE_MAX_LENGTH } from "@components/_pages/Create/constants/length";
 import DialogModalV4 from "@components/UI/DialogModalV4";
 import ImageUpload from "@components/UI/ImageUpload";
-import { ACCEPTED_FILE_TYPES } from "@components/UI/ImageUpload/utils";
-import { useUploadImageStore } from "@hooks/useUploadImage";
 import { FC, useEffect, useState } from "react";
 import EditContestNameTextInput from "../TextInput";
 
@@ -24,13 +22,11 @@ const EditContestNameModal: FC<EditContestNameModalProps> = ({
   const [inputValue, setInputValue] = useState(contestName);
   const [imageValue, setImageValue] = useState(contestImageUrl);
   const [error, setError] = useState<string>("");
-  const [uploadError, setUploadError] = useState("");
-  const [uploadSuccess, setUploadSuccess] = useState(false);
-  const { uploadImage } = useUploadImageStore();
 
   useEffect(() => {
     setInputValue(contestName);
-  }, [contestName]);
+    setImageValue(contestImageUrl);
+  }, [contestName, contestImageUrl]);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
@@ -65,52 +61,20 @@ const EditContestNameModal: FC<EditContestNameModalProps> = ({
 
     handleEditContestNameAndImage?.(value, imageValue);
     setIsCloseModal(false);
+    setInputValue("");
+    setImageValue("");
   };
 
-  const validateFile = (file: File): boolean => {
-    if (!ACCEPTED_FILE_TYPES.includes(file.type)) {
-      setUploadError("Please upload a valid image/gif file (JPEG, JPG, PNG, JFIF, GIF, or WebP)");
-      return false;
-    }
-
-    const maxSize = 20 * 1024 * 1024; // 20MB in bytes
-    if (file.size > maxSize) {
-      setUploadError("File size should be less than 20MB");
-      return false;
-    }
-
-    return true;
+  const onModalClose = () => {
+    setIsCloseModal(false);
   };
 
-  const onFileSelectHandler = async (file: File | null) => {
-    if (!file) {
-      setUploadError("");
-      setImageValue("");
-      return;
-    }
-
-    if (!validateFile(file)) {
-      return;
-    }
-
-    try {
-      const imageUrl = await uploadImageToServer(file);
-
-      setUploadSuccess(true);
-      setUploadError("");
-      setImageValue(imageUrl);
-    } catch (error) {
-      setUploadError("Failed to upload image. Please try again.");
-    }
-  };
-
-  const uploadImageToServer = async (file: File): Promise<string> => {
-    const img = await uploadImage(file);
-    return img ?? "";
+  const onImageLoadHandler = (imageUrl: string) => {
+    setImageValue(imageUrl);
   };
 
   return (
-    <DialogModalV4 isOpen={isOpen} onClose={setIsCloseModal}>
+    <DialogModalV4 isOpen={isOpen} onClose={onModalClose}>
       <div className="flex flex-col gap-14 py-6 md:py-16 pl-8 md:pl-32 pr-4 md:pr-16">
         <div className="flex justify-between items-center">
           <p className="text-[24px] text-neutral-11 font-bold">edit title and image</p>
@@ -124,12 +88,7 @@ const EditContestNameModal: FC<EditContestNameModalProps> = ({
           />
         </div>
         <div className="flex flex-col gap-2">
-          <ImageUpload
-            initialImageUrl={contestImageUrl}
-            isError={!!uploadError}
-            isSuccess={uploadSuccess}
-            onFileSelect={onFileSelectHandler}
-          />
+          <ImageUpload initialImageUrl={imageValue} onImageLoad={onImageLoadHandler} />
           <div className="bg-true-black w-full md:w-[700px] rounded-[16px] border-true-black md:shadow-file-upload md:p-4">
             <div className="bg-secondary-1 w-full rounded-[16px] outline-none p-4">
               <EditContestNameTextInput
