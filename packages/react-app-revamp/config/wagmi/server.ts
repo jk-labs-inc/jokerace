d00f5f249c0fa83dfbe65add03ed0c31fae37ec0
Siobhán McCaffery <smccaffery9@proton.me>
2025-03-22 17:04:52 -0400

    chain: add berachain

--- packages/react-app-revamp/config/wagmi/server.ts
+++ packages/react-app-revamp/config/wagmi/server.ts
@@ -5,6 +5,7 @@ import { arbitrumOne } from "./custom-chains/arbitrumOne";
 import { avalanche } from "./custom-chains/avalanche";
 import { base } from "./custom-chains/base";
 import { baseTestnet } from "./custom-chains/baseTestnet";
+import { berachain } from "./custom-chains/berachain";
 import { bnb } from "./custom-chains/bnb";
 import { celo } from "./custom-chains/celo";
 import { cyber } from "./custom-chains/cyber";
@@ -52,6 +53,7 @@ export const chains: readonly [Chain, ...Chain[]] = [
   soneium,
   story,
   ink,
+  berachain,
   sepolia,
   baseTestnet,
   mainnet,
