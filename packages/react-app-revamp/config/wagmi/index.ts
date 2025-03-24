da7ddf7b0730d0811ac8559f58b8ade83a2e7377
zho <medenicablazo@gmail.com>
2025-03-24 17:05:28 +0100

    chore: implement referer in ethers and server config (#3519)
    * chore: empty commit to trigger deployment
    
    * chore: add referrer to transports
    
    * chore: add correct link
    
    * chore: add same settings in main config
    
    * chore: rm metadata
    
    * chore: try moving out abi and version fetch from page.tsx
    
    * chore: add logging
    
    * chore: fix hook
    
    * chore: try preventing bsod
    
    * chore: add referer in ethers provider
    
    * chore: add fallback for comments
    
    * chore: revert to old flow
    
    * chore: add logging
    
    * chore: test bsod
    
    * chore: add go back instead of homepage in not found
    
    * chore: use router for not found
    
    * chore: add prod referrers

--- packages/react-app-revamp/config/wagmi/index.ts
+++ packages/react-app-revamp/config/wagmi/index.ts
@@ -1,5 +1,3 @@
-import { getParaWallet, GetParaOpts, OAuthMethod, AuthLayout } from "@getpara/rainbowkit-wallet";
-import { Environment } from "@getpara/web-sdk";
 import { Chain, connectorsForWallets } from "@rainbow-me/rainbowkit";
 import {
   argentWallet,
@@ -51,7 +49,7 @@ import { sepolia } from "./custom-chains/sepolia";
 import { soneium } from "./custom-chains/soneium";
 import { story } from "./custom-chains/story";
 import { zora } from "./custom-chains/zora";
-import { isParaWalletConfigured, paraWallet, paraWalletOpts } from "./para";
+import { isParaWalletConfigured, paraWallet } from "./para";
 
 declare module "wagmi";
 
@@ -106,7 +104,8 @@ const connectors = connectorsForWallets(
           },
         ]
       : []),
-    { groupName: "Wallets",
+    {
+      groupName: "Wallets",
       wallets: [
         metaMaskWallet,
         walletConnectWallet,
--- packages/react-app-revamp/config/wagmi/server.ts
+++ packages/react-app-revamp/config/wagmi/server.ts
@@ -30,6 +30,8 @@ import { lukso } from "./custom-chains/lukso";
 
 type Transports = Record<Chain["id"], Transport>;
 
+const isProduction = process.env.NODE_ENV === "production";
+
 export const chains: readonly [Chain, ...Chain[]] = [
   polygon,
   arbitrumOne,
@@ -60,9 +62,22 @@ export const chains: readonly [Chain, ...Chain[]] = [
 ];
 
 const createTransports = (chains: readonly [Chain, ...Chain[]]): Transports => {
+  const headers = isProduction ? { Referer: "https://jokerace.io/" } : undefined;
+
   return chains.reduce<Transports>((acc, chain) => {
     if (chain.rpcUrls?.default?.http?.[0] && chain.rpcUrls?.public?.http?.[0]) {
-      acc[chain.id] = fallback([http(chain.rpcUrls.default.http[0]), http(chain.rpcUrls.public.http[0])]);
+      acc[chain.id] = fallback([
+        http(chain.rpcUrls.default.http[0], {
+          fetchOptions: {
+            headers,
+          },
+        }),
+        http(chain.rpcUrls.public.http[0], {
+          fetchOptions: {
+            headers,
+          },
+        }),
+      ]);
     }
     return acc;
   }, {});
