8366e85aba684dc2dcc9a728dcc1c6379a600c25
zho <medenicablazo@gmail.com>
2025-04-14 16:52:16 +0200

    feat: add ability for creator/entrant to delete entry on frontend (#3624)
    * feat: add ability for creator/entrant to delete entry
    
    * feat: change position of delete button on entry page on desktop
    
    * feat: keep same dimensions on trashcan on mobile

--- packages/react-app-revamp/components/_pages/DialogModalProposal/components/Header/index.tsx
+++ packages/react-app-revamp/components/_pages/DialogModalProposal/components/Header/index.tsx
@@ -1,17 +1,21 @@
 import UserProfileDisplay from "@components/UI/UserProfileDisplay";
+import SubmissionDeleteButton from "@components/_pages/Submission/components/Buttons/Delete";
+import SubmissionDeleteModal from "@components/_pages/Submission/components/Modals/Delete";
 import { formatNumberAbbreviated } from "@helpers/formatNumber";
 import ordinalize from "@helpers/ordinalize";
 import { ProposalData } from "lib/proposal";
-import { FC } from "react";
+import { FC, useState } from "react";
 import EntryNavigation from "../EntryNavigation";
 
 interface DialogModalProposalHeaderProps {
   proposalData: ProposalData | null;
   currentIndex: number;
   totalProposals: number;
+  allowDelete: boolean;
   isProposalLoading: boolean;
   onPreviousEntry?: () => void;
   onNextEntry?: () => void;
+  onDeleteProposal?: () => void;
 }
 
 const DialogModalProposalHeader: FC<DialogModalProposalHeaderProps> = ({
@@ -19,25 +23,29 @@ const DialogModalProposalHeader: FC<DialogModalProposalHeaderProps> = ({
   currentIndex,
   totalProposals,
   isProposalLoading,
+  allowDelete,
   onPreviousEntry,
   onNextEntry,
+  onDeleteProposal,
 }) => {
+  const [isDeleteProposalModalOpen, setIsDeleteProposalModalOpen] = useState(false);
+
   if (!proposalData?.proposal) return null;
+
+  const handleDeleteProposal = () => {
+    setIsDeleteProposalModalOpen(false);
+    onDeleteProposal?.();
+  };
 
   return (
     <div className="flex flex-col gap-4 w-full">
-      {proposalData.proposal.rank > 0 && (
-        <div className="flex gap-2 items-center">
-          <p className="text-[20px] font-bold text-neutral-11">
-            {formatNumberAbbreviated(proposalData.proposal.votes)} vote
-            {proposalData.proposal.votes > 1 ? "s" : ""}
-          </p>
-          <span className="text-neutral-9">•</span>
-          <p className="text-[20px] font-bold text-neutral-9">
-            {ordinalize(proposalData.proposal.rank).label} place {proposalData.proposal.isTied ? "(tied)" : ""}
-          </p>
-        </div>
-      )}
+      {allowDelete && <SubmissionDeleteButton onClick={() => setIsDeleteProposalModalOpen(true)} />}
+
+      <div className="flex gap-2 items-center">
+        <p className="text-[20px] font-bold text-neutral-11">100 votes</p>
+        <span className="text-neutral-9">•</span>
+        <p className="text-[20px] font-bold text-neutral-9">1st place</p>
+      </div>
       <div className="flex justify-between w-full items-center">
         <UserProfileDisplay ethereumAddress={proposalData.proposal.authorEthereumAddress} shortenOnFallback={true} />
         <div className="flex items-center gap-2">
@@ -52,6 +60,11 @@ const DialogModalProposalHeader: FC<DialogModalProposalHeaderProps> = ({
           )}
         </div>
       </div>
+      <SubmissionDeleteModal
+        isDeleteProposalModalOpen={isDeleteProposalModalOpen}
+        setIsDeleteProposalModalOpen={setIsDeleteProposalModalOpen}
+        onClick={handleDeleteProposal}
+      />
     </div>
   );
 };
