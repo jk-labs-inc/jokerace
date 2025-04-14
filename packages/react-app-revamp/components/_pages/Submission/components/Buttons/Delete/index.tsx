bf3c958fbaaa33ca40877cfacd0feb6d7880bb01
blazh <medenicablazo@gmail.com>
2025-04-14 10:48:14 +0200

    feat: keep same dimensions on trashcan on mobile

--- packages/react-app-revamp/components/_pages/Submission/components/Buttons/Delete/index.tsx
+++ packages/react-app-revamp/components/_pages/Submission/components/Buttons/Delete/index.tsx
@@ -8,7 +8,7 @@ interface SubmissionDeleteButtonProps {
 const SubmissionDeleteButton: FC<SubmissionDeleteButtonProps> = ({ onClick }) => {
   return (
     <button onClick={onClick}>
-      <TrashIcon className="h-4 w-4 md:h-6 md:w-6 text-negative-11 bg-true-black hover:text-negative-10 transition-colors duration-300 ease-in-out" />
+      <TrashIcon className="h-6 w-6 text-negative-11 bg-true-black hover:text-negative-10 transition-colors duration-300 ease-in-out" />
     </button>
   );
 };
