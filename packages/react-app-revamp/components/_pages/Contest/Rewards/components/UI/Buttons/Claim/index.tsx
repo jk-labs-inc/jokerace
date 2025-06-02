6bf6fcf233045cac50d3b832800ae923e088de78
blazh <medenicablazo@gmail.com>
2025-06-02 12:21:51 +0200

    feat: add shadow animation on claim

--- packages/react-app-revamp/components/_pages/Contest/Rewards/components/UI/Buttons/Claim/index.tsx
+++ packages/react-app-revamp/components/_pages/Contest/Rewards/components/UI/Buttons/Claim/index.tsx
@@ -10,7 +10,7 @@ const RewardsClaimButton: FC<RewardsClaimButtonProps> = ({ onClick, isLoading })
     <button
       disabled={isLoading}
       onClick={onClick}
-      className={`bg-gradient-purple w-28 h-6 rounded-[40px] text-true-black text-[16px] font-bold ${
+      className={`bg-gradient-purple w-28 h-6 rounded-[40px] text-true-black text-[16px] font-bold hover:shadow-button-embossed-hover transition-all duration-300 ease-in-out ${
         isLoading ? "opacity-50 cursor-not-allowed" : ""
       }`}
     >
