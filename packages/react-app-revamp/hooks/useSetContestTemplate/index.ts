ea05d5fddc54b968e1092e10e347bd47b4479904
nakedfool <medenicablazo@gmail.com>
2025-08-04 14:38:25 +0200

    chore: rm setting submission merkle on templates

--- packages/react-app-revamp/hooks/useSetContestTemplate/index.ts
+++ packages/react-app-revamp/hooks/useSetContestTemplate/index.ts
@@ -3,15 +3,14 @@ import {
   useTimingOptionForVotingPeriod,
 } from "@components/_pages/Create/pages/ContestTiming/utils";
 import { TemplateConfig } from "@components/_pages/Create/templates/types";
-import { useDeployContestStore } from "@hooks/useDeployContest/store";
 import { ContestType } from "@components/_pages/Create/types";
-import { useSubmissionMerkle } from "@components/_pages/Create/hooks/useSubmissionMerkle";
-import { useAccount } from "wagmi";
+import { useDeployContestStore } from "@hooks/useDeployContest/store";
 import {
   DEFAULT_ALLOWED_SUBMISSIONS_PER_USER,
   MAX_ALLOWED_SUBMISSIONS_PER_USER,
   MAX_SUBMISSIONS_PER_CONTEST,
 } from "@hooks/useDeployContest/types";
+import { useAccount } from "wagmi";
 
 const useSetContestTemplate = () => {
   const { address } = useAccount();
@@ -28,11 +27,9 @@ const useSetContestTemplate = () => {
   } = useDeployContestStore(state => state);
   const { setTimingOption: setSubmissionTimingOption } = useTimingOptionForSubmissionPeriod(state => state);
   const { setTimingOption: setVotingTimingOption } = useTimingOptionForVotingPeriod(state => state);
-  const { processCreatorAllowlist } = useSubmissionMerkle();
 
   const setContestTemplateConfig = (config: TemplateConfig) => {
     if (config.data.contestType === ContestType.VotingContest) {
-      processCreatorAllowlist(address);
       setCustomization({
         maxSubmissions: MAX_SUBMISSIONS_PER_CONTEST,
         allowedSubmissionsPerUser: MAX_ALLOWED_SUBMISSIONS_PER_USER,
