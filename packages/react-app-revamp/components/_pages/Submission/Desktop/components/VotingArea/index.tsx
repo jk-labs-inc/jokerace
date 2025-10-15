911945b8d3756d8862029a411e87809c5f467f97
nakedfool <medenicablazo@gmail.com>
2025-10-15 12:40:02 +0200

    bugfix: fix max-height on voters list in entry page

--- packages/react-app-revamp/components/_pages/Submission/Desktop/components/VotingArea/index.tsx
+++ packages/react-app-revamp/components/_pages/Submission/Desktop/components/VotingArea/index.tsx
@@ -1,28 +1,30 @@
 import useContestVoteTimer, { VotingStatus } from "@components/_pages/Submission/hooks/useContestVoteTimer";
+import { useSubmissionPageStore } from "@components/_pages/Submission/store";
 import useProposalIdStore from "@hooks/useProposalId/store";
+import { useRef } from "react";
+import { useShallow } from "zustand/shallow";
+import SubmissionPageDesktopVotingAreaWidgetVotingNotOpen from "./components/Placeholder/components/VotingNotOpen";
 import SubmissionPageDesktopVotingAreaWidget from "./components/Widget";
 import SubmissionPageDesktopVotingAreaWidgetVoters from "./components/Widget/components/Voters";
-import { useSubmissionPageStore } from "@components/_pages/Submission/store";
-import SubmissionPageDesktopVotingAreaWidgetVotingNotOpen from "./components/Placeholder/components/VotingNotOpen";
-import { useShallow } from "zustand/shallow";
 
 const SubmissionPageDesktopVotingArea = () => {
   const proposalId = useProposalIdStore(useShallow(state => state.proposalId));
   const voteTimings = useSubmissionPageStore(useShallow(state => state.voteTimings));
-  const { votingStatus, isVotingOpen, timeRemaining } = useContestVoteTimer({
+  const { votingStatus, timeRemaining } = useContestVoteTimer({
     voteStart: voteTimings?.voteStart ?? null,
     contestDeadline: voteTimings?.contestDeadline ?? null,
   });
+  const containerRef = useRef<HTMLDivElement>(null);
 
   return (
-    <div className="flex flex-col p-4 gap-4 bg-primary-1 rounded-4xl xl:w-[480px] h-full">
-      {votingStatus === VotingStatus.VotingNotOpen && (
+    <div ref={containerRef} className="flex flex-col p-4 gap-4 bg-primary-1 rounded-4xl xl:w-[480px] h-full">
+      {votingStatus === VotingStatus.VotingNotOpen ? (
         <SubmissionPageDesktopVotingAreaWidgetVotingNotOpen timeRemaining={timeRemaining} />
-      )}
-      {votingStatus === VotingStatus.VotingOpen && <SubmissionPageDesktopVotingAreaWidget />}
-      {(votingStatus === VotingStatus.VotingOpen || votingStatus === VotingStatus.VotingClosed) && (
+      ) : null}
+      {votingStatus === VotingStatus.VotingOpen ? <SubmissionPageDesktopVotingAreaWidget /> : null}
+      {votingStatus === VotingStatus.VotingOpen || votingStatus === VotingStatus.VotingClosed ? (
         <SubmissionPageDesktopVotingAreaWidgetVoters proposalId={proposalId} />
-      )}
+      ) : null}
     </div>
   );
 };
