b19c5eb8b960e1fdbba82025dc418b3d51ce1c90
nakedfool <medenicablazo@gmail.com>
2025-10-22 15:17:37 +0200

    feat: apply same height as for email

--- packages/react-app-revamp/components/Voting/components/RewardsProjection/components/Container/index.tsx
+++ packages/react-app-revamp/components/Voting/components/RewardsProjection/components/Container/index.tsx
@@ -6,7 +6,7 @@ interface VotingWidgetRewardsProjectionContainerProps {
 
 const VotingWidgetRewardsProjectionContainer: FC<VotingWidgetRewardsProjectionContainerProps> = ({ children }) => {
   return (
-    <div className="flex items-center bg-transparent rounded-2xl border border-neutral-17 py-4 pl-4 pr-6 h-12">
+    <div className="flex h-14 items-center bg-transparent rounded-2xl border border-neutral-17 py-4 pl-4 pr-6">
       {children}
     </div>
   );
