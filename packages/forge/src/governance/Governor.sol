4f74a59d684c42d68d02f922b3bc9d2a349cc2b0
Siobhán McCaffery <smccaffery9@proton.me>
2025-10-24 11:06:05 -0400

    feat: always self fund logic and tests

--- packages/forge/src/governance/Governor.sol
+++ packages/forge/src/governance/Governor.sol
@@ -57,7 +57,6 @@ abstract contract Governor is GovernorSorting {
         string name;
         string prompt;
         IntConstructorArgs intConstructorArgs;
-        address creatorSplitDestination;
         address jkLabsSplitDestination;
         string metadataFieldsSchema;
     }
@@ -125,7 +124,6 @@ abstract contract Governor is GovernorSorting {
     uint256 public costToVote; // Cost per vote if flat price curve, starting/minimum price if exp curve
     uint256 public priceCurveType; // Enum value of PriceCurveTypes.
     uint256 public multiple; // Exponent multiple for an exponential price curve if applicable.
-    address public creatorSplitDestination; // Where the creator split of revenue goes.
     address public jkLabsSplitDestination; // Where the jk labs split of revenue goes.
     string public metadataFieldsSchema; // JSON Schema of what the metadata fields are.
 
@@ -151,6 +149,7 @@ abstract contract Governor is GovernorSorting {
 
     error OnlyCreatorCanSubmit();
     error ContestMustBeQueuedToPropose();
+    error OfficialModuleMustBeSetToEnter();
     error ContestMustBeActiveToVote();
     error ContestMustBeActiveToGetCurrentVotePrice();
     error SenderSubmissionLimitReached(uint256 numAllowedProposalSubmissions);
@@ -191,7 +190,6 @@ abstract contract Governor is GovernorSorting {
         costToVote = constructorArgs_.intConstructorArgs.costToVote;
         priceCurveType = constructorArgs_.intConstructorArgs.priceCurveType;
         multiple = constructorArgs_.intConstructorArgs.multiple;
-        creatorSplitDestination = constructorArgs_.creatorSplitDestination;
         jkLabsSplitDestination = constructorArgs_.jkLabsSplitDestination;
         metadataFieldsSchema = constructorArgs_.metadataFieldsSchema;
 
@@ -320,6 +318,11 @@ abstract contract Governor is GovernorSorting {
     function totalVotesCastIsZero() public virtual returns (bool totalVotesCastZero);
 
     /**
+     * @dev Returns the official rewards module.
+     */
+    function getOfficialRewardsModule() public view virtual returns (address officialRewardsModule);
+
+    /**
      * @dev Register a vote with a given support and voting weight.
      */
     function _countVote(uint256 proposalId, address account, uint256 numVotes) internal virtual;
@@ -412,8 +415,8 @@ abstract contract Governor is GovernorSorting {
 
             uint256 sendingToCreator = msg.value - sendingToJkLabs;
             if (sendingToCreator > 0) {
-                Address.sendValue(payable(creatorSplitDestination), sendingToCreator); // creator gets the extra wei in the case of rounding
-                emit CreatorPaymentReleased(creator, sendingToCreator);
+                Address.sendValue(payable(getOfficialRewardsModule()), sendingToCreator); // creator gets the extra wei in the case of rounding
+                emit CreatorPaymentReleased(getOfficialRewardsModule(), sendingToCreator);
             }
         }
     }
@@ -424,6 +427,7 @@ abstract contract Governor is GovernorSorting {
     function propose(ProposalCore calldata proposal) public payable returns (uint256) {
         uint256 actionCost = _determineCorrectAmountSent(Actions.Submit, 0);
 
+        if (getOfficialRewardsModule() == address(0)) revert OfficialModuleMustBeSetToEnter();
         verifyProposer();
         validateProposalData(proposal);
         uint256 proposalId = _castProposal(proposal);
@@ -530,14 +534,6 @@ abstract contract Governor is GovernorSorting {
         emit VoteCast(account, proposalId, numVotes);
 
         return numVotes;
-    }
-
-    function setCreatorSplitDestination(address newCreatorSplitDestination) public {
-        if (msg.sender != creator) revert OnlyCreatorCanAmend();
-        if (state() == ContestState.Completed || state() == ContestState.Canceled) {
-            revert CannotUpdateWhenCompletedOrCanceled();
-        }
-        creatorSplitDestination = newCreatorSplitDestination;
     }
 
     function setJkLabsSplitDestination(address newJkLabsSplitDestination) public {
