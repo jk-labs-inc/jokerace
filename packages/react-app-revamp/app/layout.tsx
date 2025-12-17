15217f1c91d9a9c4f54b6ef79ce285be42b8e425
zho <medenicablazo@gmail.com>
2025-12-17 14:54:11 +0100

    chore: preload fonts in layout (#5030)

--- packages/react-app-revamp/app/layout.tsx
+++ packages/react-app-revamp/app/layout.tsx
@@ -7,12 +7,51 @@ import { polyfill } from "interweave-ssr";
 import { GA_TRACKING_ID } from "lib/gtag";
 import { Metadata, Viewport } from "next";
 import dynamic from "next/dynamic";
+import localFont from "next/font/local";
 import { headers } from "next/headers";
 import NextTopLoader from "nextjs-toploader";
 import "react-loading-skeleton/dist/skeleton.css";
 import "react-tooltip/dist/react-tooltip.css";
 import "simplebar-react/dist/simplebar.min.css";
 import Providers from "./providers";
+
+const lato = localFont({
+  src: [
+    {
+      path: "./_fonts/Lato-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./_fonts/Lato-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "./_fonts/Lato-Black.woff2",
+      weight: "900",
+      style: "normal",
+    },
+  ],
+  display: "swap",
+  variable: "--font-family-lato",
+});
+
+const saboRegular = localFont({
+  src: "../public/fonts/Sabo-Regular.otf",
+  weight: "400",
+  style: "normal",
+  display: "swap",
+  variable: "--font-family-sabo-regular",
+});
+
+const saboFilled = localFont({
+  src: "./_fonts/Sabo-Filled.otf",
+  weight: "400",
+  style: "normal",
+  display: "swap",
+  variable: "--font-family-sabo-filled",
+});
 
 polyfill();
 
@@ -49,8 +88,8 @@ export default async function RootLayout({ children }: { children: React.ReactNo
   const cookie = headersList.get("cookie") ?? "";
 
   return (
-    <html lang="en">
-      <body>
+    <html lang="en" className={`${lato.variable} ${saboRegular.variable} ${saboFilled.variable} antialiased`}>
+      <body className={lato.className}>
         <div id="__next">
           <NextTopLoader color="#BB65FF" shadow="0 0 10px #BB65FF, 0 0 5px #78FFC6" showSpinner={false} />
           <Providers cookie={cookie}>
